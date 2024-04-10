135025956c4004c29726849e3296137d28d90e0c
thoulee <thou_lee@outlook.com>
2024-04-10 17:55:55 +0800

    remove backdrop in BottomSheetPaper

--- src/components/BottomSheetPaper.tsx
+++ src/components/BottomSheetPaper.tsx
@@ -1,9 +1,5 @@
-import BottomSheet, {
-    BottomSheetBackdrop,
-    BottomSheetBackdropProps,
-    BottomSheetView
-} from "@gorhom/bottom-sheet";
-import React, { PropsWithChildren, useCallback } from "react";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { PropsWithChildren } from "react";
 import { StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
 
@@ -11,23 +7,13 @@ export const BottomSheetPaper = ({ bottomSheetRef, children }:
     PropsWithChildren<{ bottomSheetRef: React.RefObject<BottomSheet> }>
 ) => {
     const appTheme = useTheme();
-
-    const renderBackdrop = useCallback(
-        (backdropProps: BottomSheetBackdropProps) =>
-            <BottomSheetBackdrop
-                {...backdropProps}
-                disappearsOnIndex={-1}
-                appearsOnIndex={0}
-            />, []);
-
     return <BottomSheet
         ref={bottomSheetRef}
-        index={2}
+        index={-1}
         backgroundStyle={{ backgroundColor: appTheme.colors.background }}
         handleIndicatorStyle={{ backgroundColor: appTheme.colors.primary }}
         snapPoints={['40%', '80%', '97%']}
         enablePanDownToClose
-        backdropComponent={renderBackdrop}
         android_keyboardInputMode="adjustResize"
         enableOverDrag={false}//防止与FlatList（ScrollView）冲突
     >
