f829e8ca78b4eef653276ec289d566d878927d0c
thoulee <thou_lee@outlook.com>
2024-04-16 23:16:25 +0800

    Refactor CommentList component to include pull-to-refresh functionality

--- src/pages/comments.tsx
+++ src/pages/comments.tsx
@@ -1,9 +1,22 @@
 import Clipboard from '@react-native-clipboard/clipboard';
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, ToastAndroid } from "react-native";
+import {
+    FlatList,
+    RefreshControl,
+    SafeAreaView,
+    StyleSheet,
+    ToastAndroid
+} from "react-native";
 import HapticFeedback from "react-native-haptic-feedback";
-import { ActivityIndicator, Appbar, Avatar, Divider, List, useTheme } from 'react-native-paper';
+import {
+    ActivityIndicator,
+    Appbar,
+    Avatar,
+    Divider,
+    List,
+    useTheme
+} from 'react-native-paper';
 import { useActiveTrack } from "react-native-track-player";
 import { requestInit } from "../services";
 import { Comment, Main } from '../types/comments';
@@ -14,25 +27,34 @@ function CommentList() {
 
     const [comments, setComments] = useState<Comment[]>([]);
     const [isEmpty, setIsEmpty] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
 
     const id = track?.id;
-    const limit = 30;
+    const limit = 20;
     const offset = 1;
 
+    const fetchComments = async () => {
+        const response = await fetch(
+            `http://music.163.com/api/v1/resource/comments/R_SO_4_${id}?limit=${limit}&offset=${offset}`,
+            requestInit
+        );
+        const data: Main = await response.json();
+
+        setIsEmpty(data.total === 0);
+        setComments(data.comments);
+    };
+
     useEffect(() => {
-        const fetchComments = async () => {
-            const response = await fetch(
-                `http://music.163.com/api/v1/resource/comments/R_SO_4_${id}?limit=${limit}&offset=${offset}`,
-                requestInit
-            );
-
-            const data: Main = await response.json();
-            setIsEmpty(data.total === 0);
-            setComments(data.comments);
-        };
-
         fetchComments();
     }, [id]);
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        fetchComments()
+            .then(() => {
+                setRefreshing(false);
+            });
+    }
 
     if (isEmpty) {
         return (
@@ -43,28 +65,39 @@ function CommentList() {
         );
     }
 
+    const renderComment = ({ item }: { item: Comment }) =>
+        <List.Item
+            title={item.user.nickname}
+            description={item.content}
+            titleStyle={{ color: appTheme.colors.secondary }}
+            descriptionNumberOfLines={10}
+            left={props =>
+                <Avatar.Image {...props}
+                    source={{ uri: item.user.avatarUrl }}
+                />
+            }
+            onLongPress={() => {
+                Clipboard.setString(item.content);
+                HapticFeedback.trigger('effectClick');
+                ToastAndroid.show('Copied to clipboard', ToastAndroid.SHORT);
+            }}
+        />
+
     return (
         <FlatList
             data={comments}
-            keyExtractor={(item) => item.commentId.toString()}
-            renderItem={({ item }) =>
-                <List.Item
-                    title={item.user.nickname}
-                    description={item.content}
-                    titleStyle={{ color: appTheme.colors.secondary }}
-                    descriptionNumberOfLines={10}
-                    left={props =>
-                        <Avatar.Image {...props}
-                            source={{ uri: item.user.avatarUrl }}
-                        />
-                    }
-                    onLongPress={() => {
-                        Clipboard.setString(item.content);
-                        HapticFeedback.trigger('effectClick');
-                        ToastAndroid.show('Copied to clipboard', ToastAndroid.SHORT);
-                    }}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    colors={[appTheme.colors.primary]}
+                    progressBackgroundColor={appTheme.colors.background}
                 />
             }
+            keyExtractor={(item) => item.commentId.toString()}
+            renderItem={renderComment}
             ItemSeparatorComponent={() => <Divider />}
             // Component to render when loading data
             ListEmptyComponent={() =>
