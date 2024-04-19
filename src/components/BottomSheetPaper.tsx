26fea0533ff6a4e67744f5faf7ac7812511f2466
thoulee <thou_lee@outlook.com>
2024-04-19 14:04:37 +0800

    Refactor BottomSheetPaper component to use appTheme colors and adjust handle indicator

--- src/components/BottomSheetPaper.tsx
+++ src/components/BottomSheetPaper.tsx
@@ -1,18 +1,26 @@
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import Color from 'color';
 import React, { PropsWithChildren } from 'react';
 import { StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 export const BottomSheetPaper = ({
   bottomSheetRef,
   children,
 }: PropsWithChildren<{ bottomSheetRef: React.RefObject<BottomSheet> }>) => {
+  const appTheme = useTheme();
+
   return (
     <BottomSheet
       ref={bottomSheetRef}
       index={-1}
       backgroundStyle={styles.transparent}
-      handleIndicatorStyle={styles.transparent}
-      snapPoints={['99%']}
+      handleIndicatorStyle={{
+        backgroundColor:
+          Color(appTheme.colors.onSurface)
+            .fade(0.2).string()
+      }}
+      snapPoints={['97%']}
       enablePanDownToClose
       android_keyboardInputMode="adjustResize"
       enableOverDrag={false} //防止与FlatList（ScrollView）冲突
