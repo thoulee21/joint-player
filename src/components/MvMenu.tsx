7cbfec158f524a8cf0b720bd900eca9624b22425
thoulee <thou_lee@outlook.com>
2024-04-27 15:47:12 +0800

    Refactor MvMenu component disabled behavior

--- src/components/MvMenu.tsx
+++ src/components/MvMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Linking } from 'react-native';
 import { Menu } from 'react-native-paper';
 import TrackPlayer, { useActiveTrack } from 'react-native-track-player';
@@ -8,26 +8,22 @@ export function MvMenu({ onPostPressed }:
   { onPostPressed: () => void }
 ) {
   const track = useActiveTrack();
-  const [disabled, setDisabled] = useState(true);
 
   const { data: mvData, isLoading, error } = useSWR(
     track ? `https://music.163.com/api/mv/detail?id=${track?.mvid}&type=mp4` : null,
   );
 
-  useEffect(() => {
-    if (track && track?.mvid) {
-      setDisabled(track?.mvid === 0);
-    } else {
-      setDisabled(true);
-    }
-  }, [track]);
+  const disabled =
+    track?.mvid === 0
+    || typeof track?.mvid === 'undefined'
+    || isLoading || error;
 
   return (
     <Menu.Item
       title="Watch MV"
       leadingIcon="video-outline"
       trailingIcon={isLoading ? 'loading' : undefined}
-      disabled={disabled || isLoading || error}
+      disabled={disabled}
       onPress={async () => {
         const highRes = Object.keys(mvData.data.brs).reverse()[0];
         const mv = mvData.data.brs[highRes];
