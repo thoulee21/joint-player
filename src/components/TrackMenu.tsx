d4c66d52e17102b26aaadae909eda92c353ba06e
thoulee <thou_lee@outlook.com>
2024-04-28 21:47:35 +0800

    update TrackMenu's style

--- src/components/TrackMenu.tsx
+++ src/components/TrackMenu.tsx
@@ -5,23 +5,25 @@ import { CommentsMenu, MvMenu } from '.';
 
 export function TrackMenu(props: any) {
   const navigation = useNavigation();
-  const [visible, setVisible] = useState(false);
 
+  const [visible, setVisible] = useState(false);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
 
   return (
     <Menu
       visible={visible}
+      elevation={1}
       onDismiss={closeMenu}
       anchor={
         <IconButton
-          size={24}
-          icon="dots-vertical-circle-outline"
-          onPress={openMenu}
           {...props}
+          size={24}
+          icon="dots-vertical"
+          onPress={openMenu}
         />
-      }>
+      }
+    >
       <MvMenu
         onPostPressed={closeMenu}
         navigation={navigation}
