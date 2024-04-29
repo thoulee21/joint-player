1211da519d201aa852667c45bf2c26523883eaf4
thoulee <thou_lee@outlook.com>
2024-04-29 16:01:41 +0800

    Update MV player to use HapticFeedback and update StatusBar styles

--- src/pages/mv.tsx
+++ src/pages/mv.tsx
@@ -3,13 +3,19 @@ import { useNavigation } from '@react-navigation/native';
 import { OrientationLock, lockAsync } from 'expo-screen-orientation';
 import React, { useEffect } from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
+import HapticFeedBack, {
+    HapticFeedbackTypes,
+} from 'react-native-haptic-feedback';
 import VideoPlayer from 'react-native-media-console';
+import { useTheme } from 'react-native-paper';
 import { useActiveTrack } from 'react-native-track-player';
 import useSWR from 'swr';
 import { Main as MvMain } from '../types/mv';
 
 export const MvPlayer = () => {
     const navigator = useNavigation();
+    const appTheme = useTheme();
+
     const track = useActiveTrack();
 
     const { data } = useSWR<MvMain>(
@@ -22,14 +28,23 @@ export const MvPlayer = () => {
         ? data.data.brs[highestRes as keyof typeof data.data.brs]
         : 'https://vjs.zencdn.net/v/oceans.mp4';
 
+    const vibration = () => {
+        HapticFeedBack.trigger(HapticFeedbackTypes.effectHeavyClick);
+    };
+
     useEffect(() => {
         lockAsync(OrientationLock.LANDSCAPE);
         StatusBar.setHidden(true);
+        StatusBar.setBarStyle('light-content');
 
         return () => {
             lockAsync(OrientationLock.PORTRAIT);
             StatusBar.setHidden(false);
+            StatusBar.setBarStyle(appTheme.dark
+                ? 'light-content'
+                : 'dark-content');
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
@@ -46,6 +61,12 @@ export const MvPlayer = () => {
             title={data?.data.name || 'MV'}
             controlTimeoutDelay={2000}
             preventsDisplaySleepDuringVideoPlayback
+            tapAnywhereToPause
+            seekColor={appTheme.colors.primary}
+            onShowControls={() => StatusBar.setHidden(false)}
+            onHideControls={() => StatusBar.setHidden(true)}
+            onPlay={vibration}
+            onPause={vibration}
         />
     );
 };
