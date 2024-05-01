54542d3d5d22cc3e4362ea5256140651d9ce482b
thoulee <thou_lee@outlook.com>
2024-05-01 18:02:39 +0800

    Refactor comments screen and components to use BlurBackground

--- src/pages/comments.tsx
+++ src/pages/comments.tsx
@@ -1,38 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
-import { BlurView } from 'expo-blur';
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
-import { Appbar, useTheme } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Appbar } from 'react-native-paper';
 import { useActiveTrack } from 'react-native-track-player';
-import { CommentList, placeholderImg } from '../components';
-import { useAppSelector } from '../hook/reduxHooks';
-import { blurRadius } from '../redux/slices';
+import { BlurBackground, CommentList } from '../components';
 
 export function Comments(): React.JSX.Element {
     const navigation = useNavigation();
-    const appTheme = useTheme();
-
     const track = useActiveTrack();
-    const blurRadiusValue = useAppSelector(blurRadius);
 
     return (
-        <ImageBackground
-            style={styles.rootView}
-            source={{ uri: track?.artwork || placeholderImg }}
-            blurRadius={blurRadiusValue}
-        >
-            <BlurView
-                tint={appTheme.dark ? 'dark' : 'light'}
-                style={styles.rootView}
-            >
-                <Appbar.Header style={styles.header}>
-                    <Appbar.BackAction onPress={navigation.goBack} />
-                    <Appbar.Content title="Comments" />
-                </Appbar.Header>
+        <BlurBackground>
+            <Appbar.Header style={styles.header}>
+                <Appbar.BackAction onPress={navigation.goBack} />
+                <Appbar.Content title="Comments" />
+            </Appbar.Header>
 
-                <CommentList commentThreadId={`R_SO_4_${track?.id}`} />
-            </BlurView>
-        </ImageBackground>
+            <CommentList commentThreadId={`R_SO_4_${track?.id}`} />
+        </BlurBackground>
     );
 }
 
