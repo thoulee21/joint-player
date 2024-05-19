0bd38047cd1e4f83108913c5d6f79eacef2d3113
thoulee <thou_lee@outlook.com>
2024-05-19 23:14:40 +0800

    refactor: Update SongItem to become a general purpose component

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,19 +1,30 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
 import { IconButton, List, useTheme } from 'react-native-paper';
 import TrackPlayer, { useActiveTrack, useIsPlaying } from 'react-native-track-player';
 import { useAppDispatch } from '../hook';
 import { clearQueue, addToQueue as reduxAddQueue } from '../redux/slices';
+import { TrackType } from '../services';
 import { Song } from '../types/albumDetail';
 import { songToTrack } from '../utils';
 
-export function SongItem({ item }: { item: Song }) {
+export function SongItem({ item, track }: { item?: Song, track?: TrackType }) {
     const dispatch = useAppDispatch();
     const appTheme = useTheme();
     const currentTrack = useActiveTrack();
     const { playing } = useIsPlaying();
 
-    const trackData = useMemo(() => songToTrack(item), [item]);
+    let trackData: TrackType;
+    if (!track) {
+        if (item) {
+            trackData = songToTrack(item);
+        } else {
+            throw new Error('SongItem must have either a song or a track');
+        }
+    } else {
+        trackData = track;
+    }
+
     const active = (currentTrack?.id === trackData.id) && playing;
 
     const play = async () => {
@@ -35,7 +46,7 @@ export function SongItem({ item }: { item: Song }) {
         const existTracks = await TrackPlayer.getQueue();
         if (existTracks) {
             const exist = existTracks.findIndex(
-                (track) => track.id === trackData.id
+                (trackItem) => trackItem.id === trackData.id
             );
             if (exist === -1) {
                 await TrackPlayer.add(trackData);
@@ -55,8 +66,8 @@ export function SongItem({ item }: { item: Song }) {
                         : undefined}
                 />
             )}
-            title={item.name}
-            description={item.artists.map(ar => ar.name).join(', ')}
+            title={trackData?.title}
+            description={trackData?.artists.map(ar => ar.name).join(', ')}
             onPress={play}
             right={(props) => (
                 <IconButton {...props}
