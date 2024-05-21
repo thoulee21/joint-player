343a64772a932eb9a3b2934122c796ddafac49d5
thoulee <thou_lee@outlook.com>
2024-05-21 23:37:54 +0800

    refactor: Add right prop to SongItem component for custom rendering

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
 import { IconButton, List, useTheme } from 'react-native-paper';
+import { Style } from 'react-native-paper/lib/typescript/components/List/utils';
 import TrackPlayer, { useActiveTrack, useIsPlaying } from 'react-native-track-player';
 import { useAppDispatch } from '../hook';
 import { addToQueueAsync, clearAddOneAsync } from '../redux/slices';
@@ -8,7 +9,16 @@ import { TrackType } from '../services';
 import { Song } from '../types/albumDetail';
 import { songToTrack } from '../utils';
 
-export function SongItem({ item, track }: { item?: Song, track?: TrackType }) {
+export interface ListRightProps {
+    color: string;
+    style?: Style;
+}
+
+export function SongItem({ item, track, right }: {
+    item?: Song,
+    track?: TrackType,
+    right: (props: ListRightProps) => ReactNode
+}) {
     const dispatch = useAppDispatch();
     const appTheme = useTheme();
 
@@ -55,8 +65,8 @@ export function SongItem({ item, track }: { item?: Song, track?: TrackType }) {
             title={trackData?.title}
             description={trackData?.artists.map(ar => ar.name).join(', ')}
             onPress={play}
-            right={(props) => (
-                <IconButton {...props}
+            right={(props) => right ? right(props) : (
+                <IconButton
                     icon="music-note-plus"
                     onPress={addToQueue}
                 />
