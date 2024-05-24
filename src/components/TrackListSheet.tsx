7b6355cf2475737a5003254467f085479ea83552
thoulee <thou_lee@outlook.com>
2024-05-24 16:09:03 +0800

    fix: restore TrackListSheet component to handle TrackPlayer queue asynchronously

--- src/components/TrackListSheet.tsx
+++ src/components/TrackListSheet.tsx
@@ -1,13 +1,12 @@
-import BottomSheet, {
-  BottomSheetFlatList
-} from '@gorhom/bottom-sheet';
-import React from 'react';
+import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Portal } from 'react-native-paper';
-import { Track } from 'react-native-track-player';
+import TrackPlayer, { Track, useActiveTrack } from 'react-native-track-player';
 import { BottomSheetPaper, TrackItem } from '.';
-import { useAppSelector } from '../hook';
-import { queue } from '../redux/slices';
+import { useAppDispatch, useAppSelector } from '../hook';
+import { queue, setQueue } from '../redux/slices';
+import { TrackType } from '../services';
 
 interface TrackListProps {
   bottomSheetRef: React.RefObject<BottomSheet>;
@@ -17,7 +16,24 @@ interface TrackListProps {
 function TrackList({
   bottomSheetRef, navigation
 }: TrackListProps) {
+  const dispatch = useAppDispatch();
+  const currentTrack = useActiveTrack();
   const tracks = useAppSelector(queue);
+
+  useEffect(() => {
+    async function getQueue() {
+      try {
+        const playerQueue = await TrackPlayer.getQueue();
+        if (playerQueue) {
+          dispatch(setQueue(playerQueue as TrackType[]));
+        }
+      } catch { } // ignore player errors
+    }
+
+    getQueue();
+    // no dispatch
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTrack]);
 
   const renderTrack = ({ item, index }:
     { item: Track; index: number }
@@ -40,10 +56,7 @@ function TrackList({
       data={tracks}
       ListEmptyComponent={() => (
         <View>
-          <ActivityIndicator
-            size="large"
-            style={styles.loading}
-          />
+          <ActivityIndicator size="large" style={styles.loading} />
         </View>
       )}
       renderItem={renderTrack}
