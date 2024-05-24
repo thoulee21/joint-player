d27fc28efd59b0dcb87c873bdf2656f975793547
thoulee <thou_lee@outlook.com>
2024-05-24 18:52:12 +0800

    refactor: extract IndexOfSong from SongItem

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,11 +1,33 @@
 import * as Sentry from '@sentry/react-native';
 import React, { memo } from 'react';
 import { ViewStyle } from 'react-native';
+import HapticFeedback, {
+    HapticFeedbackTypes
+} from 'react-native-haptic-feedback';
 import { List, Text, useTheme } from 'react-native-paper';
+import { Style } from 'react-native-paper/lib/typescript/components/List/utils';
 import TrackPlayer from 'react-native-track-player';
 import { useAppDispatch } from '../hook';
 import { clearAddOneAsync } from '../redux/slices';
 import { TrackType } from '../services';
+
+const IndexOfSong = memo(({ style: leftStyle, index }: {
+    style: Style, index: number
+}) => {
+    const appTheme = useTheme();
+    return (
+        <Text
+            variant="titleLarge"
+            style={[leftStyle, {
+                color: appTheme.dark
+                    ? appTheme.colors.onSurfaceDisabled
+                    : appTheme.colors.backdrop
+            }]}
+        >
+            {index + 1}
+        </Text>
+    );
+});
 
 export const SongItem = memo(({ item, index, style }: {
     item: TrackType,
@@ -16,6 +38,9 @@ export const SongItem = memo(({ item, index, style }: {
     const appTheme = useTheme();
 
     const play = async () => {
+        HapticFeedback.trigger(
+            HapticFeedbackTypes.effectHeavyClick
+        );
         await dispatch(clearAddOneAsync(item));
         await TrackPlayer.play();
     };
@@ -23,17 +48,8 @@ export const SongItem = memo(({ item, index, style }: {
     return (
         <Sentry.ErrorBoundary showDialog>
             <List.Item
-                left={({ style: leftStyle }) => (
-                    <Text
-                        variant="titleLarge"
-                        style={[leftStyle, {
-                            color: appTheme.dark
-                                ? appTheme.colors.onSurfaceDisabled
-                                : appTheme.colors.backdrop
-                        }]}
-                    >
-                        {index + 1}
-                    </Text>
+                left={(props) => (
+                    <IndexOfSong {...props} index={index} />
                 )}
                 title={item.title}
                 description={
