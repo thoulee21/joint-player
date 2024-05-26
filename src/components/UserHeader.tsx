0a05bd69d7445b56b51c6016350ddb091958ad89
thoulee <thou_lee@outlook.com>
2024-05-26 14:16:49 +0800

    feat: add view img function to UserHeader.tsx

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,8 +1,22 @@
-import { useNetInfoInstance } from '@react-native-community/netinfo';
 import { useNavigation } from '@react-navigation/native';
 import React, { memo } from 'react';
-import { Dimensions, ImageBackground, StatusBar, StyleSheet } from 'react-native';
-import { ActivityIndicator, Avatar, Text, TouchableRipple, useTheme } from 'react-native-paper';
+import {
+    Dimensions,
+    ImageBackground,
+    StatusBar,
+    StyleSheet,
+    TouchableWithoutFeedback
+} from 'react-native';
+import HapticFeedback, {
+    HapticFeedbackTypes
+} from 'react-native-haptic-feedback';
+import {
+    ActivityIndicator,
+    Avatar,
+    Text,
+    TouchableRipple,
+    useTheme
+} from 'react-native-paper';
 import useSWR from 'swr';
 import { useAppSelector } from '../hook';
 import { selectUser } from '../redux/slices';
@@ -12,9 +26,7 @@ export const UserHeader = memo(({ userId }: { userId?: number }) => {
     const navigation = useNavigation();
     const appTheme = useTheme();
 
-    const { netInfo } = useNetInfoInstance();
     const currentUser = useAppSelector(selectUser);
-
     const { data, error, isLoading } = useSWR<Main>(
         `https://music.163.com/api/v1/user/detail/${userId || currentUser.id}`,
     );
@@ -32,35 +44,67 @@ export const UserHeader = memo(({ userId }: { userId?: number }) => {
         );
     }
 
+    const viewBackground = () => {
+        HapticFeedback.trigger(
+            HapticFeedbackTypes.effectDoubleClick
+        );
+        if (data) {
+            //@ts-expect-error
+            navigation.push('WebView', {
+                url: data?.profile.backgroundUrl,
+                title: 'User Background'
+            });
+        }
+    };
+
+    const viewAvatar = () => {
+        HapticFeedback.trigger(
+            HapticFeedbackTypes.effectDoubleClick
+        );
+        if (data) {
+            //@ts-expect-error
+            navigation.push('WebView', {
+                url: data?.profile.avatarUrl,
+                title: 'Avatar'
+            });
+        }
+    };
+
     return (
-        <ImageBackground
-            style={styles.header}
-            imageStyle={styles.img}
-            source={{ uri: data?.profile.backgroundUrl }}
+        <TouchableWithoutFeedback
+            onLongPress={viewBackground}
         >
-            <TouchableRipple
-                style={styles.avatar}
-                borderless
-                onPress={() => {
-                    if (netInfo.isConnected) {
+            <ImageBackground
+                style={styles.header}
+                imageStyle={styles.img}
+                source={{ uri: data?.profile.backgroundUrl }}
+            >
+                <TouchableRipple
+                    style={styles.avatar}
+                    borderless
+                    onPress={() => {
+                        HapticFeedback.trigger(
+                            HapticFeedbackTypes.effectHeavyClick
+                        );
                         //@ts-expect-error
                         navigation.push('Login');
-                    }
-                }}
-            >
-                <Avatar.Image
-                    size={70}
-                    source={{ uri: data?.profile.avatarUrl }}
-                />
-            </TouchableRipple>
+                    }}
+                    onLongPress={viewAvatar}
+                >
+                    <Avatar.Image
+                        size={70}
+                        source={{ uri: data?.profile.avatarUrl }}
+                    />
+                </TouchableRipple>
 
-            <Text variant="labelLarge">
-                {data?.profile.nickname}
-            </Text>
-            <Text variant="labelMedium">
-                {data?.profile.signature}
-            </Text>
-        </ImageBackground>
+                <Text variant="labelLarge">
+                    {data?.profile.nickname}
+                </Text>
+                <Text variant="labelMedium">
+                    {data?.profile.signature}
+                </Text>
+            </ImageBackground>
+        </TouchableWithoutFeedback>
     );
 });
 
