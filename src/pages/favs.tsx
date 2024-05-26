30863fde5694acc594545b665d647f00fa23e229
thoulee <thou_lee@outlook.com>
2024-05-26 16:35:11 +0800

    feat: Add clear favorites confirmation dialog

--- src/pages/favs.tsx
+++ src/pages/favs.tsx
@@ -1,18 +1,56 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import HapticFeedback, {
     HapticFeedbackTypes
 } from 'react-native-haptic-feedback';
-import { Appbar } from 'react-native-paper';
-import { BlurBackground } from '../components';
-import { FavsList } from '../components/FavsList';
-import { useAppDispatch } from '../hook';
-import { clearFavs } from '../redux/slices';
+import { Appbar, Button, Dialog, Text } from 'react-native-paper';
+import { BlurBackground, FavsList } from '../components';
+import { useAppDispatch, useAppSelector } from '../hook';
+import { clearFavs, favs } from '../redux/slices';
+
+const ConfirmClearFavsDialog = ({ visible, hideDialog }: {
+    visible: boolean;
+    hideDialog: () => void;
+}) => {
+    const dispatch = useAppDispatch();
+
+    const confirmClearFavorites = () => {
+        HapticFeedback.trigger(
+            HapticFeedbackTypes.effectTick
+        );
+        hideDialog();
+        dispatch(clearFavs());
+    };
+
+    return (
+        <Dialog
+            visible={visible}
+            onDismiss={hideDialog}
+            dismissable={false}
+            dismissableBackButton
+        >
+            <Dialog.Icon icon="alert" size={40} />
+            <Dialog.Title>Clear Favorites</Dialog.Title>
+            <Dialog.Content>
+                <Text>
+                    Are you sure you want to clear all favorites?
+                </Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={hideDialog}>Cancel</Button>
+                <Button onPress={confirmClearFavorites}>
+                    OK
+                </Button>
+            </Dialog.Actions>
+        </Dialog>
+    );
+};
 
 export function Favs() {
-    const dispatch = useAppDispatch();
     const navigation = useNavigation();
+    const favsValue = useAppSelector(favs);
+    const [dialogVisible, setDialogVisible] = useState(false);
 
     return (
         <BlurBackground>
@@ -30,11 +68,21 @@ export function Favs() {
                 <Appbar.Content title="Favorites" />
                 <Appbar.Action
                     icon="delete-forever-outline"
-                    onPress={() => { dispatch(clearFavs()); }}
+                    disabled={favsValue.length === 0}
+                    onPress={() => {
+                        HapticFeedback.trigger(
+                            HapticFeedbackTypes.notificationWarning
+                        );
+                        setDialogVisible(true);
+                    }}
                 />
             </Appbar.Header>
 
             <FavsList />
+            <ConfirmClearFavsDialog
+                visible={dialogVisible}
+                hideDialog={() => setDialogVisible(false)}
+            />
         </BlurBackground>
     );
 }
