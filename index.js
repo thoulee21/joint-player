4da731fead08d0253971e54185659f9b55273d4c
thoulee <thou_lee@outlook.com>
2024-05-31 19:48:15 +0800

    chore: remove expo eas services config files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import TrackPlayer from 'react-native-track-player';
 import { Provider } from 'react-redux';
-import { name as appName } from './app.json';
+import { name as appName } from './package.json';
 import App from './src/App';
 import { store } from './src/redux/store';
 import { PlaybackService } from './src/services';
