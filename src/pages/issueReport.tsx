35c6fe1edc554698f4391f71a5961738cec44d7e
thoulee <thou_lee@outlook.com>
2024-09-30 15:43:42 +0800

    refactor: update issue report page UI
    - Update the UI of the issue report page by adding a new input field for the issue description and an optional email field.
    - Improve the styling of the Appbar.Header component.
    - Add a BlurBackground component to the page.
    - Remove unnecessary code and improve readability in the UserItem component.

--- src/pages/issueReport.tsx
+++ src/pages/issueReport.tsx
@@ -2,11 +2,12 @@ import Clipboard from '@react-native-clipboard/clipboard';
 import { useNavigation } from '@react-navigation/native';
 import * as Sentry from '@sentry/react-native';
 import { UserFeedback } from '@sentry/react-native';
+import Color from 'color';
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, ToastAndroid, View } from 'react-native';
+import { KeyboardAvoidingView, ScrollView, StyleSheet, ToastAndroid, View } from 'react-native';
 import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
 import { Appbar, HelperText, TextInput, useTheme } from 'react-native-paper';
-import { VersionItem } from '../components';
+import { BlurBackground } from '../components';
 import { useAppSelector } from '../hook';
 import { selectUser } from '../redux/slices';
 
@@ -21,6 +22,8 @@ export const IssueReport = () => {
     const [email, setEmail] = useState('');
 
     const sentryId = Sentry.lastEventId() || Sentry.captureMessage('Report Issue');
+    const emailHasErrors = (!email.includes('@') || !email.includes('.')) && email.length > 0;
+    const sendable = issue && !emailHasErrors;
 
     const report = () => {
         const feedback: UserFeedback = {
@@ -29,24 +32,72 @@ export const IssueReport = () => {
             email: email,
             comments: issue,
         };
-
         Sentry.captureUserFeedback(feedback);
+
         HapticFeedback.trigger(HapticFeedbackTypes.effectTick);
         ToastAndroid.show('Issue reported', ToastAndroid.SHORT);
     };
 
-    const emailHasErrors = () => {
-        return (!email.includes('@') || !email.includes('.')) && email.length > 0;
+    const inputBackgroundColor = Color(appTheme.colors.secondaryContainer).fade(0.7).string();
+
+    const IssueField = () => {
+        return (
+            <View style={styles.inputField}>
+                <TextInput
+                    label="Issue Description"
+                    multiline
+                    numberOfLines={10}
+                    value={issue}
+                    onChangeText={setIssue}
+                    placeholder="Please describe the issue you encountered"
+                    maxLength={ISSUE_MAX_LENGTH}
+                    right={<TextInput.Affix
+                        text={`${issue.length}/${ISSUE_MAX_LENGTH}`}
+                    />}
+                    style={{ backgroundColor: inputBackgroundColor }}
+                />
+                <HelperText
+                    type="info"
+                    onLongPress={() => {
+                        Clipboard.setString(sentryId);
+                        ToastAndroid.show('Event ID copied', ToastAndroid.SHORT);
+                        HapticFeedback.trigger(HapticFeedbackTypes.effectDoubleClick);
+                    }}
+                >
+                    Event ID: {sentryId}
+                </HelperText>
+            </View>
+        );
     };
-    const sendable = issue && !emailHasErrors();
+
+    const EmailField = () => {
+        return (
+            <View style={styles.inputField}>
+                <TextInput
+                    label="Email (Optional)"
+                    value={email}
+                    onChangeText={setEmail}
+                    placeholder="Please enter your email address"
+                    keyboardType="email-address"
+                    textContentType="emailAddress"
+                    error={emailHasErrors}
+                    right={email && <TextInput.Icon
+                        icon="close"
+                        onPress={() => { setEmail(''); }}
+                    />}
+                    style={{ backgroundColor: inputBackgroundColor }}
+                />
+                <HelperText type="error" visible={emailHasErrors}>
+                    Email address is invalid!
+                </HelperText>
+            </View>
+        );
+    };
 
     return (
-        <View>
-            <Appbar.Header>
-                <Appbar.Action
-                    icon="arrow-left"
-                    onPress={navigation.goBack}
-                />
+        <BlurBackground>
+            <Appbar.Header style={styles.appbar} mode="large">
+                <Appbar.BackAction onPress={navigation.goBack} />
                 <Appbar.Content title="Report Issue" />
                 <Appbar.Action
                     icon={sendable ? 'send' : 'send-outline'}
@@ -56,63 +107,23 @@ export const IssueReport = () => {
                 />
             </Appbar.Header>
 
-            <ScrollView>
-                <VersionItem />
-
-                <View style={styles.input}>
-                    <TextInput
-                        label="Issue Description"
-                        multiline
-                        numberOfLines={10}
-                        value={issue}
-                        onChangeText={setIssue}
-                        placeholder="Please describe the issue you encountered"
-                        maxLength={ISSUE_MAX_LENGTH}
-                        right={
-                            <TextInput.Affix
-                                text={`${issue.length}/${ISSUE_MAX_LENGTH}`}
-                            />
-                        }
-                    />
-                    <HelperText
-                        type="info"
-                        onLongPress={() => {
-                            Clipboard.setString(sentryId);
-                            ToastAndroid.show('Event ID copied', ToastAndroid.SHORT);
-                            HapticFeedback.trigger(HapticFeedbackTypes.effectDoubleClick);
-                        }}
-                    >
-                        Event ID: {sentryId}
-                    </HelperText>
-                </View>
-
-                <View style={styles.input}>
-                    <TextInput
-                        label="Email (Optional)"
-                        value={email}
-                        onChangeText={setEmail}
-                        placeholder="Please enter your email address"
-                        keyboardType="email-address"
-                        textContentType="emailAddress"
-                        error={emailHasErrors()}
-                        right={
-                            email && <TextInput.Icon
-                                icon="close"
-                                onPress={() => { setEmail(''); }}
-                            />
-                        }
-                    />
-                    <HelperText type="error" visible={emailHasErrors()}>
-                        Email address is invalid!
-                    </HelperText>
-                </View>
-            </ScrollView>
-        </View>
+            <KeyboardAvoidingView behavior="padding">
+                <ScrollView>
+                    <IssueField />
+                    <EmailField />
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </BlurBackground>
     );
+
+
 };
 
 const styles = StyleSheet.create({
-    input: {
+    appbar: {
+        backgroundColor: 'transparent',
+    },
+    inputField: {
         marginHorizontal: 16,
         marginVertical: 4,
     },
