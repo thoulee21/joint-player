ea7b7dfa2292e383f1a43790e895df6ae03298f1
thoulee <thou_lee@outlook.com>
2024-10-03 19:43:57 +0800

    feat: add appbar mode switch in Comments component

--- src/pages/comments.tsx
+++ src/pages/comments.tsx
@@ -1,5 +1,5 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { BlurBackground, CommentList } from '../components';
@@ -8,11 +8,17 @@ export function Comments(): React.JSX.Element {
     const navigation = useNavigation();
     const { commentThreadId } = useRoute().params as { commentThreadId: string };
 
+    const [large, setLarge] = useState(false);
+
     return (
         <BlurBackground>
-            <Appbar.Header style={styles.header} mode="large">
+            <Appbar.Header style={styles.header} mode={large ? 'large' : 'small'}>
                 <Appbar.BackAction onPress={navigation.goBack} />
                 <Appbar.Content title="Comments" />
+                <Appbar.Action
+                    icon={large ? 'chevron-up' : 'chevron-down'}
+                    onPress={() => setLarge(prev => !prev)}
+                />
             </Appbar.Header>
 
             <CommentList commentThreadId={commentThreadId} />
