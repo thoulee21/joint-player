e33213137e8b45aef1364ae1c96271ecd9cdba1d
thoulee <thou_lee@outlook.com>
2024-10-11 21:00:59 +0800

    Refactor DrawerItemList component to import StyleSheet from react-native and add margin to drawer items

--- src/components/DrawerItemList.tsx
+++ src/components/DrawerItemList.tsx
@@ -10,7 +10,7 @@ import {
 } from '@react-navigation/native';
 import Color from 'color';
 import React, { useCallback } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { Drawer, useTheme } from 'react-native-paper';
 
 type Props = {
@@ -25,71 +25,72 @@ type Props = {
 function DrawerItems({ state, navigation, descriptors }: Props) {
     const appTheme = useTheme();
 
-    const renderDrawerItem = useCallback(() => {
-        return ({ item, index }: { item: any, index: number }) => {
-            const focused = index === state.index;
-            const onPress = () => {
-                const event = navigation.emit({
-                    type: 'drawerItemPress',
-                    target: item.key,
-                    canPreventDefault: true,
+    const renderDrawerItem = useCallback(({ item, index }: {
+        item: any, index: number
+    }) => {
+        const focused = index === state.index;
+        const onPress = () => {
+            const event = navigation.emit({
+                type: 'drawerItemPress',
+                target: item.key,
+                canPreventDefault: true,
+            });
+
+            if (!event.defaultPrevented) {
+                navigation.dispatch({
+                    ...(focused
+                        ? DrawerActions.closeDrawer()
+                        : CommonActions.navigate({
+                            name: item.name,
+                            merge: true
+                        })),
+                    target: state.key,
                 });
-
-                if (!event.defaultPrevented) {
-                    navigation.dispatch({
-                        ...(focused
-                            ? DrawerActions.closeDrawer()
-                            : CommonActions.navigate({
-                                name: item.name,
-                                merge: true
-                            })),
-                        target: state.key,
-                    });
-                }
-            };
-
-            const {
-                title, drawerIcon,
-            } = descriptors[item.key].options;
-
-            const backgroundColor = focused
-                ? Color(appTheme.colors.secondaryContainer)
-                    .fade(appTheme.dark ? 0.4 : 0.6).string()
-                : undefined;
-
-            const icon = () => {
-                if (typeof drawerIcon === 'function') {
-                    return drawerIcon({
-                        focused,
-                        color: focused
-                            ? appTheme.colors.primary
-                            : appTheme.colors.onSurface,
-                        size: 24,
-                    });
-                }
-                return drawerIcon;
-            };
-
-            return (
-                <Drawer.Item
-                    key={item.key}
-                    label={title !== undefined
-                        ? title
-                        : item.name}
-                    icon={icon}
-                    onPress={onPress}
-                    active={focused}
-                    style={{ backgroundColor }}
-                />
-            );
+            }
         };
-    }, [appTheme.colors.onSurface, appTheme.colors.primary, appTheme.colors.secondaryContainer, appTheme.dark, descriptors, navigation, state.index, state.key]);
+
+        const { title, drawerIcon } = descriptors[item.key].options;
+
+        const backgroundColor = focused
+            ? Color(appTheme.colors.secondaryContainer)
+                .fade(appTheme.dark ? 0.4 : 0.6).string()
+            : undefined;
+
+        const icon = () => {
+            if (typeof drawerIcon === 'function') {
+                return drawerIcon({
+                    focused,
+                    color: focused
+                        ? appTheme.colors.primary
+                        : appTheme.colors.onSurface,
+                    size: 24,
+                });
+            }
+            return drawerIcon;
+        };
+
+        return (
+            <Drawer.Item
+                key={item.key}
+                label={title !== undefined
+                    ? title
+                    : item.name}
+                icon={icon}
+                onPress={onPress}
+                active={focused}
+                style={[styles.drawerItem, {
+                    backgroundColor
+                }]}
+            />
+        );
+    }, [appTheme, descriptors, navigation, state]);
 
     return (
         <FlatList
             data={state.routes}
-            renderItem={renderDrawerItem()}
+            renderItem={renderDrawerItem}
             keyExtractor={item => item.key}
+            extraData={state.index}
         />
     );
 }
@@ -101,3 +102,9 @@ export function DrawerItemList(props: Props) {
         </Drawer.Section>
     );
 }
+
+const styles = StyleSheet.create({
+    drawerItem: {
+        marginVertical: 2,
+    },
+});
