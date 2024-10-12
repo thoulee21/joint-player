1eefbd7f53645772d226b7999c5a64bfd930dd43
thoulee <thou_lee@outlook.com>
2024-10-12 14:10:32 +0800

    chore(babel.config.js): remove console print in production

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
   plugins: [
     'react-native-reanimated/plugin',
     [
