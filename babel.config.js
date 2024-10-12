6142c7a4b8257bfaac84a7c4e0d68b24dfd8e13f
thoulee <thou_lee@outlook.com>
2024-10-12 14:00:05 +0800

    chore(babel.config.js): add ahooks import plugin

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'import',
+      {
+        libraryName: 'ahooks',
+        camel2DashComponentName: false, // 是否需要驼峰转短线
+        camel2UnderlineComponentName: false, // 是否需要驼峰转下划线
+      }
+    ]
+  ],
 };
