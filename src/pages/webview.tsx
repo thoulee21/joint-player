17742e4c45ed1a4f9ada8292ebcca6f905982cec
thoulee <thou_lee@outlook.com>
2024-10-13 22:23:07 +0800

    fix(webview): add status bar padding to webview container

--- src/pages/webview.tsx
+++ src/pages/webview.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
 import React, { useCallback, useRef, useState } from 'react';
-import { StyleSheet, ToastAndroid, View } from 'react-native';
+import { StatusBar, StyleSheet, ToastAndroid, View } from 'react-native';
 import { Appbar, ProgressBar, useTheme } from 'react-native-paper';
 import { WebView } from 'react-native-webview';
 import {
@@ -86,5 +86,6 @@ export const WebViewScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight,
   },
 });
