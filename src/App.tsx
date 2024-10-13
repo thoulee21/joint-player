0dcc08c96dc790f6bb5e36ae1a126449a919d52e
thoulee <thou_lee@outlook.com>
2024-10-13 18:10:14 +0800

    refactor(App): add expo-updates pending ToastAndroid

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as SplashScreen from 'expo-splash-screen';
+import { useUpdates } from 'expo-updates';
 import React, { useEffect } from 'react';
+import { ToastAndroid } from 'react-native';
 import { AppContainer } from './components/AppContainer';
 import { RootStack } from './components/RootStack';
 import { useAppDispatch } from './hook/reduxHooks';
@@ -9,6 +11,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const dispatch = useAppDispatch();
+  const { isUpdatePending } = useUpdates();
 
   useEffect(() => {
     Promise.all([
@@ -18,6 +21,17 @@ export default function App() {
     ]);
 
     // no dispatch
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (isUpdatePending) {
+      ToastAndroid.show(
+        'An update is pending. Restart the app to apply the update.',
+        ToastAndroid.SHORT
+      );
+    }
+    // show toast at startup
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
