3a5a1fd17767129a60c5b4dd8612a04cc7951c93
thoulee <thou_lee@outlook.com>
2024-10-14 18:44:24 +0800

    refactor(metro.config.js): Update watchFolders and useWatchman options
    Update the metro.config.js file to include the watchFolders and useWatchman options in the resolver configuration. This allows the project to watch for changes in the 'src' folder and use Watchman for file system monitoring.

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig: getDefaultExpoConfig } = require('expo/metro-config');
 const { mergeConfig, getDefaultConfig } = require('@react-native/metro-config');
 const { withSentryConfig } = require('@sentry/react-native/metro');
+const path = require('path');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const expoDefaultConfig = getDefaultExpoConfig(__dirname);
@@ -11,6 +12,14 @@ const expoDefaultConfig = getDefaultExpoConfig(__dirname);
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = mergeConfig(defaultConfig, expoDefaultConfig);
+const config = mergeConfig(defaultConfig, expoDefaultConfig, {
+    resolver: {
+        watchFolders: [
+            // Add your project's source folder path here
+            path.resolve(__dirname, 'src')
+        ],
+        useWatchman: true,
+    },
+});
 
 module.exports = withSentryConfig(config);
