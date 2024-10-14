2d7ab196ab99a60a7fc2a14133f73a647b45bce8
thoulee <thou_lee@outlook.com>
2024-10-14 19:16:09 +0800

    refactor: Remove unused babel plugin and update react-native-paper version

--- babel.config.js
+++ babel.config.js
@@ -5,15 +5,5 @@ module.exports = {
       plugins: ['transform-remove-console'],
     },
   },
-  plugins: [
-    'react-native-reanimated/plugin',
-    [
-      'import',
-      {
-        libraryName: 'ahooks',
-        camel2DashComponentName: false, // 是否需要驼峰转短线
-        camel2UnderlineComponentName: false, // 是否需要驼峰转下划线
-      }
-    ]
-  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
