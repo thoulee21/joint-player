0e1daa71fc678566a2d38ae702a868585128a628
thoulee <thou_lee@outlook.com>
2024-10-14 16:26:33 +0800

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
-    // 忽略换行符错误
-    'prettier/prettier': 0,
-    'react/no-unstable-nested-components': 0,
-    'react-hooks/exhaustive-deps': 1,
-    'no-unused-vars': 1,
-    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
     'comma-dangle': 0,
   },
 };
