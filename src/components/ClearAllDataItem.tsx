a89db9e7a371731fed367538fd2217cac9e06ddc
thoulee <thou_lee@outlook.com>
2024-11-01 22:44:47 +0800

    refactor: replace ugly Alert with material paper Dialog

--- src/components/ClearAllDataItem.tsx
+++ src/components/ClearAllDataItem.tsx
@@ -1,49 +1,59 @@
-import React, { useCallback } from 'react';
-import { Alert } from 'react-native';
-import HapticFeedback, {
-    HapticFeedbackTypes,
-} from 'react-native-haptic-feedback';
-import { List, useTheme } from 'react-native-paper';
+import React, { useCallback, useState } from 'react';
+import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
+import { Button, Dialog, List, Portal, Text, useTheme } from 'react-native-paper';
 import RNRestart from 'react-native-restart';
 import { Storage } from '../utils';
 
 export const ClearAllDataItem = () => {
-    const appTheme = useTheme();
+  const appTheme = useTheme();
 
-    const DeleteForeverIcon = useCallback((props: any) => (
-        <List.Icon {...props}
-            icon="delete-forever-outline"
-            color={appTheme.colors.error}
-        />
-    ), [appTheme.colors.error]);
+  const [visible, setVisible] = useState(false);
+  const showDialog = useCallback(() => setVisible(true), []);
+  const hideDialog = useCallback(() => setVisible(false), []);
 
-    const clearAndRestart = useCallback(async () => {
-        await Storage.clear();
-        RNRestart.Restart();
-    }, []);
+  const DeleteForeverIcon = useCallback((props: any) => (
+    <List.Icon {...props}
+      icon="delete-forever-outline"
+      color={appTheme.colors.error}
+    />
+  ), [appTheme.colors.error]);
 
-    const clearAllData = useCallback(async () => {
-        HapticFeedback.trigger(HapticFeedbackTypes.notificationWarning);
-        Alert.alert(
-            'Clear All Data',
-            'Are you sure you want to clear all data?',
-            [
-                { text: 'Cancel', style: 'cancel' },
-                {
-                    text: 'OK',
-                    style: 'destructive',
-                    onPress: clearAndRestart,
-                },
-            ],
-        );
-    }, [clearAndRestart]);
+  const clearAndRestart = useCallback(async () => {
+    await Storage.clear();
+    RNRestart.Restart();
+  }, []);
 
-    return (
-        <List.Item
-            title="Clear All Data"
-            description="Clear all data and restart the app"
-            left={DeleteForeverIcon}
-            onPress={clearAllData}
-        />
-    );
+  const clearAllData = useCallback(async () => {
+    HapticFeedback.trigger(HapticFeedbackTypes.notificationWarning);
+    showDialog();
+  }, [showDialog]);
+
+  return (
+    <>
+      <List.Item
+        title="Clear All Data"
+        description="Clear all data and restart the app"
+        left={DeleteForeverIcon}
+        onPress={clearAllData}
+      />
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>Clear All Data</Dialog.Title>
+          <Dialog.Content>
+            <Text>
+              Are you sure you want to clear all data?
+            </Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>
+              Cancel
+            </Button>
+            <Button onPress={clearAndRestart}>
+              OK
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
+  );
 };
