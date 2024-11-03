fd85450c5cab9c755da9157d30aabaf666b96f5a
thoulee <thou_lee@outlook.com>
2024-11-03 15:25:41 +0800

    feat(App): 配置 Reanimated 日志记录器以提高调试能力

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import * as Sentry from '@sentry/react-native';
 import React, { useEffect } from 'react';
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
 import TrackPlayer from 'react-native-track-player';
 import { AppContainer } from './components/AppContainer';
 import { RootStack } from './components/RootStack';
 import { useAppDispatch } from './hook/reduxHooks';
 import { initBlurRadius, initFavs, initUser } from './redux/slices';
 import { PlaybackService } from './services/PlaybackService';
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.error,
+  strict: false, // Reanimated runs in strict mode by default
+});
 
 Sentry.init({
   dsn: 'https://8f255799dc215a920003a9291d1d1e14@o4507198225383424.ingest.de.sentry.io/4507198229184592',
