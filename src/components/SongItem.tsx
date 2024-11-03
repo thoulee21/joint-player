337d60d17a9a811f4da7e866ad6e831effa7e42e
thoulee <thou_lee@outlook.com>
2024-11-03 21:24:22 +0800

    feat(SongItem): 提取索引组件并优化渲染逻辑

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,96 +1,79 @@
 import React, { useCallback, useMemo } from 'react';
 import { ViewStyle } from 'react-native';
-import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
-import { List, Text, useTheme } from 'react-native-paper';
-import { Style } from 'react-native-paper/lib/typescript/components/List/utils';
+import HapticFeedback, {
+  HapticFeedbackTypes,
+} from 'react-native-haptic-feedback';
+import { List, useTheme } from 'react-native-paper';
 import TrackPlayer from 'react-native-track-player';
 import { useAppDispatch } from '../hook';
 import { clearAddOneAsync } from '../redux/slices';
 import { TrackType } from '../services/GetTracksService';
 import type { ListLRProps } from '../types/paperListItem';
-
-const IndexOfSong = ({ style: leftStyle, index }: {
-    style?: Style, index: number
-}) => {
-    const appTheme = useTheme();
-    return (
-        <Text
-            variant="titleLarge"
-            style={[leftStyle, {
-                color: appTheme.dark
-                    ? appTheme.colors.onSurfaceDisabled
-                    : appTheme.colors.backdrop,
-            }]}
-        >
-            {index + 1}
-        </Text>
-    );
-};
+import { IndexOfSong } from './IndexOfSong';
 
 export const SongItem = ({
-    item,
-    index,
-    style,
-    showAlbum,
-    showIndex,
-    onLongPress,
-    isActive,
+  item,
+  index,
+  style,
+  showAlbum,
+  showIndex,
+  onLongPress,
+  isActive,
 }: {
-    item: TrackType,
-    index: number,
-    style?: ViewStyle,
-    showAlbum?: boolean,
-    showIndex?: boolean,
-    onLongPress?: () => void,
-    isActive?: boolean,
+  item: TrackType,
+  index: number,
+  style?: ViewStyle,
+  showAlbum?: boolean,
+  showIndex?: boolean,
+  onLongPress?: () => void,
+  isActive?: boolean,
 }) => {
-    const dispatch = useAppDispatch();
-    const appTheme = useTheme();
+  const dispatch = useAppDispatch();
+  const appTheme = useTheme();
 
-    const play = useCallback(async () => {
-        HapticFeedback.trigger(
-            HapticFeedbackTypes.effectHeavyClick
-        );
-        await dispatch(clearAddOneAsync(item));
-        await TrackPlayer.play();
-        //no dispatch
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [item]);
+  const songStyle = useMemo(() => ([
+    style, {
+      backgroundColor: isActive
+        ? appTheme.colors.primaryContainer
+        : appTheme.colors.surface,
+    }]
+  ), [style, isActive, appTheme]);
 
-    const renderIndex = useCallback((props: ListLRProps) => {
-        if (showIndex) {
-            return (
-                <IndexOfSong {...props} index={index} />
-            );
-        }
-    }, [index, showIndex]);
-
-    const description = useMemo(() => {
-        const artists = item.artists
-            .map(ar => ar.name)
-            .join(', ');
-
-        if (showAlbum) {
-            return artists
-                .concat(' - ', item.album);
-        } else {
-            return artists;
-        }
-    }, [item.album, item.artists, showAlbum]);
-
-    return (
-        <List.Item
-            left={renderIndex}
-            title={item.title}
-            description={description}
-            descriptionNumberOfLines={1}
-            onPress={play}
-            onLongPress={onLongPress}
-            style={[style, {
-                backgroundColor: isActive
-                    ? appTheme.colors.primaryContainer
-                    : appTheme.colors.surface,
-            }]}
-        />
+  const play = useCallback(async () => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectHeavyClick
     );
+    await dispatch(clearAddOneAsync(item));
+    await TrackPlayer.play();
+    //no dispatch
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [item]);
+
+  const renderIndex = useCallback((props: ListLRProps) => (
+    showIndex && <IndexOfSong {...props} index={index} />
+  ), [index, showIndex]);
+
+  const description = useMemo(() => {
+    const artists = item.artists
+      .map(ar => ar.name)
+      .join(', ');
+
+    if (showAlbum) {
+      return artists.concat(' - ', item.album);
+    } else {
+      return artists;
+    }
+  }, [item.album, item.artists, showAlbum]);
+
+  return (
+    <List.Item
+      left={renderIndex}
+      title={item.title}
+      description={description}
+      descriptionNumberOfLines={1}
+      onPress={play}
+      onLongPress={onLongPress}
+      style={songStyle}
+    />
+  );
 };
