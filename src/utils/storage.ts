e3fb4d342165d3a43f17ab4e4c3c8943cb98fb01
thoulee <thou_lee@outlook.com>
2024-11-03 23:23:17 +0800

    refactor(storage): 将 Storage 类导出方式更改为默认导出

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export class Storage {
+export default class Storage {
   //直接传js值，不必转换字符串
   static set(key: string, value: any) {
     return AsyncStorage.setItem(key, JSON.stringify(value));
@@ -29,5 +29,3 @@ export class Storage {
     return AsyncStorage.clear();
   }
 }
-
-export default Storage;
