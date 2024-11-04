cd3ad6abe37422f5fe2bba06ed42b900887e19a1
thoulee <thou_lee@outlook.com>
2024-11-04 23:00:00 +0800

    feat(SwitchUser): 添加用户头像显示和搜索功能，优化用户体验

--- src/pages/switchUser.tsx
+++ src/pages/switchUser.tsx
@@ -1,54 +1,108 @@
 import { useNavigation } from '@react-navigation/native';
+import Color from 'color';
 import React, { useCallback, useState } from 'react';
-import { StatusBar, StyleSheet } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Avatar, IconButton, Searchbar, useTheme } from 'react-native-paper';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import useSWR from 'swr';
 import { BlurBackground } from '../components/BlurBackground';
 import { LottieAnimation } from '../components/LottieAnimation';
 import { UserList } from '../components/UserList';
+import { useAppSelector } from '../hook';
+import { selectUser } from '../redux/slices';
+import type { Main as UserMain } from '../types/userDetail';
 
 export const SwitchUser = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+  const appTheme = useTheme();
 
-    const [showQuery, setShowQuery] = useState('');
-    const [searchQuery, setSearchQuery] = useState('');
+  const currentUser = useAppSelector(selectUser);
+  const [showQuery, setShowQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
-    const search = useCallback(() => {
-        if (showQuery && searchQuery !== showQuery) {
-            setSearchQuery(showQuery);
-        }
-    }, [searchQuery, showQuery]);
+  const { data, error, isLoading: isAvatarLoading } = useSWR<UserMain>(
+    `https://music.163.com/api/v1/user/detail/${currentUser.id}`,
+  );
+
+  const renderAvatar = useCallback((props: any) => {
+    if (error || isAvatarLoading) { return null; }
 
     return (
-        <BlurBackground>
-            <Searchbar
-                style={styles.searchbar}
-                placeholder="Search for a user"
-                onChangeText={setShowQuery}
-                value={showQuery}
-                icon="arrow-left"
-                onIconPress={navigation.goBack}
-                onSubmitEditing={search}
-                selectTextOnFocus
-                onClearIconPress={() => {
-                    setSearchQuery('');
-                    setShowQuery('');
-                }}
-            />
-
-            {searchQuery
-                ? <UserList searchQuery={searchQuery} />
-                : <LottieAnimation
-                    caption="Login to use your custom settings"
-                    animation="rocket"
-                />}
-        </BlurBackground>
+      <View style={styles.rightBtns}>
+        {showQuery && (
+          <IconButton
+            {...props}
+            icon="close"
+            onPress={() => {
+              setSearchQuery('');
+              setShowQuery('');
+            }}
+            iconColor={appTheme.dark
+              ? appTheme.colors.onSurfaceDisabled
+              : appTheme.colors.backdrop}
+          />
+        )}
+        <Avatar.Image
+          {...props}
+          size={40}
+          source={{ uri: data?.profile.avatarUrl }}
+        />
+      </View>
     );
+  }, [
+    appTheme,
+    data?.profile.avatarUrl,
+    error,
+    isAvatarLoading,
+    showQuery,
+  ]);
+
+  const search = useCallback(() => {
+    if (showQuery && searchQuery !== showQuery) {
+      setSearchQuery(showQuery);
+    }
+  }, [searchQuery, showQuery]);
+
+  return (
+    <BlurBackground>
+      <Searchbar
+        style={[styles.searchbar, {
+          backgroundColor: Color(
+            appTheme.colors.secondaryContainer
+          ).fade(0.3).string(),
+          marginTop: insets.top,
+        }]}
+        placeholder="Search for a user"
+        onChangeText={setShowQuery}
+        value={showQuery}
+        icon="arrow-left"
+        right={renderAvatar}
+        onIconPress={navigation.goBack}
+        onSubmitEditing={search}
+        selectTextOnFocus
+        blurOnSubmit
+        selectionColor={
+          appTheme.colors.inversePrimary
+        }
+      />
+
+      {searchQuery
+        ? <UserList searchQuery={searchQuery} />
+        : <LottieAnimation
+          caption="Login to use your custom settings"
+          animation="rocket"
+        />}
+    </BlurBackground>
+  );
 };
 
 const styles = StyleSheet.create({
-    searchbar: {
-        marginTop: StatusBar.currentHeight,
-        marginHorizontal: '2%',
-        backgroundColor: 'transparent',
-    },
+  searchbar: {
+    marginHorizontal: '2%',
+  },
+  rightBtns: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
