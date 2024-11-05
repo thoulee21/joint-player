532d69e8e46b4e229ab02fdc54b24ecd04f95ab1
thoulee <thou_lee@outlook.com>
2024-11-05 23:32:48 +0800

    feat(SearchSongList): 优化搜索歌曲列表组件，改进加载状态和错误处理，提升用户体验

--- src/components/SearchSongList.tsx
+++ src/components/SearchSongList.tsx
@@ -1,97 +1,124 @@
 import { FlashList } from '@shopify/flash-list';
 import React, { useCallback, useMemo, useState } from 'react';
-import { RefreshControl, StyleSheet } from 'react-native';
-import { ActivityIndicator, useTheme } from 'react-native-paper';
+import { Dimensions, RefreshControl, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Text, useTheme } from 'react-native-paper';
 import useSWRInfinite from 'swr/infinite';
 import type { Main, Song } from '../types/searchSongs';
 import { LottieAnimation } from './LottieAnimation';
 import { SearchSongItem } from './SearchSongItem';
 
+const Type = 1;
+const itemsPerPage = 15;
+
 export const SearchSongList = ({ keyword }: { keyword: string }) => {
-    const appTheme = useTheme();
-    const [refreshing, setRefreshing] = useState(false);
+  const appTheme = useTheme();
+  const [refreshing, setRefreshing] = useState(false);
 
-    const {
-        data, error, isLoading, mutate, setSize,
-    } = useSWRInfinite<Main>((index) => {
-        const Type = 1;
-        const itemsPerPage = 15;
-        const Offset = index * itemsPerPage;
-        const Limit = itemsPerPage;
-
-        return (
-            `https://music.163.com/api/search/get/web?csrf_token=hlpretag=&hlposttag=&s=${keyword}&type=${Type}&offset=${Offset}&total=true&limit=${Limit}`
-        );
-    });
-
-    const showData = useMemo(() => data?.flatMap(
-        (item) => item.result.songs
-    ) || [], [data]);
-
-    const loadMore = useCallback(() => {
-        setSize((prev) => prev + 1);
-    }, [setSize]);
-
-    const keyExtractor = useCallback(
-        (item: Song) => item.id.toString(), []
-    );
-
-    const onRefresh = useCallback(async () => {
-        setRefreshing(true);
-        await mutate();
-        setRefreshing(false);
-        // no mutate
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const renderItem = useCallback((
-        props: { index: number, item: Song }
-    ) => (
-        <SearchSongItem {...props} />
-    ), []);
-
-    const renderEmpty = useCallback(() => (
-        isLoading && (
-            <ActivityIndicator
-                size="large"
-                style={styles.loading}
-            />
-        )
-    ), [isLoading]);
-
+  const {
+    data, error, isLoading, mutate, setSize,
+  } = useSWRInfinite<Main>((index) => {
+    const Offset = index * itemsPerPage;
+    const Limit = itemsPerPage;
     return (
-        !error ? (
-            <FlashList
-                data={showData}
-                keyExtractor={keyExtractor}
-                renderItem={renderItem}
-                onEndReachedThreshold={0.1}
-                onEndReached={loadMore}
-                refreshing={refreshing}
-                onRefresh={onRefresh}
-                fadingEdgeLength={100}
-                estimatedItemSize={84}
-                ListEmptyComponent={renderEmpty}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                        colors={[appTheme.colors.primary]}
-                        progressBackgroundColor={appTheme.colors.surface}
-                    />
-                }
-            />
-        ) : (
-            <LottieAnimation
-                caption="Failed to load songs"
-                animation="breathe"
-            />
-        )
+      `https://music.163.com/api/search/get/web?csrf_token=hlpretag=&hlposttag=&s=${keyword}&type=${Type}&offset=${Offset}&total=true&limit=${Limit}`
     );
+  });
+
+  const showData = useMemo(() => {
+    const notFound = data?.map(
+      (item) => item.result.songCount
+    ).reduce(
+      (a, b) => a + b, 0
+    ) === 0;
+
+    if (!notFound) {
+      return data?.flatMap(
+        (item) => item.result.songs
+      ) || [];
+    } else { return []; }
+  }, [data]);
+
+  const loadMore = useCallback(() => {
+    setSize((prev) => prev + 1);
+  }, [setSize]);
+
+  const keyExtractor = useCallback(
+    (item: Song) => item.id.toString(), []
+  );
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await mutate();
+    setRefreshing(false);
+    // no mutate
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const renderItem = useCallback((
+    props: { index: number, item: Song }
+  ) => (
+    <SearchSongItem {...props} />
+  ), []);
+
+  const renderEmpty = useCallback(() => (
+    <View style={styles.root}>
+      {isLoading
+        ? <ActivityIndicator size="large" style={styles.loading} />
+        : <LottieAnimation caption="No songs found" animation="teapot" />}
+    </View>
+  ), [isLoading]);
+
+  return (
+    !error ? (
+      <FlashList
+        data={showData}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        onEndReachedThreshold={0.1}
+        onEndReached={loadMore}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+        fadingEdgeLength={100}
+        estimatedItemSize={84}
+        ListEmptyComponent={renderEmpty}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[appTheme.colors.primary]}
+            progressBackgroundColor={appTheme.colors.surface}
+          />
+        }
+      />
+    ) : (
+      <LottieAnimation
+        caption={error.message}
+        animation="breathe"
+      >
+        <Text
+          variant="bodyLarge"
+          style={[styles.errTxt, {
+            color: appTheme.colors.error,
+          }]}
+        >
+          Failed to load songs
+        </Text>
+      </LottieAnimation>
+    )
+  );
 };
 
 const styles = StyleSheet.create({
-    loading: {
-        marginTop: '40%',
-    },
+  root: {
+    height: Dimensions.get('window').height - 84,
+    width: Dimensions.get('window').width,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errTxt: {
+    textAlign: 'center',
+  },
 });
