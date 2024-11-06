30ba4678ec1389b0699256c742033a34610a04ce
thoulee <thou_lee@outlook.com>
2024-11-06 21:18:18 +0800

    feat(RootStack): 重构导航结构，优化屏幕分组和动画效果

--- src/components/RootStack.tsx
+++ src/components/RootStack.tsx
@@ -1,8 +1,7 @@
 import {
-    StackCardInterpolationProps,
     StackNavigationOptions,
     TransitionPresets,
-    createStackNavigator,
+    createStackNavigator
 } from '@react-navigation/stack';
 import React from 'react';
 import {
@@ -25,46 +24,33 @@ import { DrawerNavi } from './DrawerNavi';
 
 const Stack = createStackNavigator();
 
-// 弹性动画
-const flexAnimation = ({ current, layouts }:
-    StackCardInterpolationProps
-) => ({
-    cardStyle: {
-        transform: [
-            {
-                translateX: current.progress.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [layouts.screen.width, 0],
-                }),
-            },
-        ],
-    },
-});
-
 const screenOptions: StackNavigationOptions = {
     headerShown: false,
     ...TransitionPresets.SlideFromRightIOS,
-    cardStyleInterpolator: flexAnimation,
 };
 
 export function RootStack() {
     return (
         <Stack.Navigator screenOptions={screenOptions}>
             <Stack.Screen name="DrawerNavi" component={DrawerNavi} />
-            <Stack.Screen name="WebView" component={WebViewScreen} />
             <Stack.Screen name="Comments" component={Comments} />
             <Stack.Screen name="Lyrics" component={LyricsScreen} />
             <Stack.Screen name="MvPlayer" component={MvPlayer} />
             <Stack.Screen name="MvDetail" component={MvDetail} />
             <Stack.Screen name="Artist" component={Artist} />
-            <Stack.Screen name="AlbumDetail" component={AlbumDetail} />
-            <Stack.Screen name="IssueReport" component={IssueReport} />
             <Stack.Screen name="Dev" component={DevScreen} />
             <Stack.Screen name="About" component={AboutScreen} />
             <Stack.Screen name="AppData" component={AppDataScreen} />
-            <Stack.Screen name="SwitchUser" component={SwitchUser} />
             <Stack.Screen name="AniGallery" component={AniGallery} />
-            <Stack.Screen name="Search" component={Search} />
+            <Stack.Group
+                screenOptions={TransitionPresets.ModalPresentationIOS}
+            >
+                <Stack.Screen name="IssueReport" component={IssueReport} />
+                <Stack.Screen name="AlbumDetail" component={AlbumDetail} />
+                <Stack.Screen name="SwitchUser" component={SwitchUser} />
+                <Stack.Screen name="WebView" component={WebViewScreen} />
+                <Stack.Screen name="Search" component={Search} />
+            </Stack.Group>
         </Stack.Navigator>
     );
 }
