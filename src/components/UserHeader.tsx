acd446dc0af7e8e38bc191b0c4485e581058d70e
thoulee <thou_lee@outlook.com>
2024-11-07 22:00:16 +0800

    refactor(UserHeader): 优化用户信息组件，增加调试功能，简化样式和逻辑

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,176 +1,197 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { type PropsWithChildren } from 'react';
 import {
-    ImageBackground,
-    StatusBar,
-    StyleSheet,
-    TouchableWithoutFeedback,
-    View,
+  Alert,
+  ImageBackground,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  type ImageStyle,
+  type StyleProp,
+  type ViewStyle
 } from 'react-native';
 import HapticFeedback, {
-    HapticFeedbackTypes,
+  HapticFeedbackTypes,
 } from 'react-native-haptic-feedback';
 import {
-    ActivityIndicator,
-    Avatar,
-    Text,
-    TouchableRipple,
-    useTheme,
+  ActivityIndicator,
+  Avatar,
+  Text,
+  TouchableRipple,
+  useTheme,
 } from 'react-native-paper';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import useSWR from 'swr';
 import { useAppSelector } from '../hook';
-import { selectUser } from '../redux/slices';
+import { selectDevModeEnabled, selectUser } from '../redux/slices';
 import { Main } from '../types/userDetail';
 import { placeholderImg } from './TrackInfo';
 
-export const UserInfo = ({ userId }: { userId?: number }) => {
-    const navigation = useNavigation();
-    const appTheme = useTheme();
-    const currentUser = useAppSelector(selectUser);
+export const UserInfo = ({ userId, style }: {
+  userId?: number, style?: StyleProp<ViewStyle>
+}) => {
+  const navigation = useNavigation();
+  const appTheme = useTheme();
 
-    const { data } = useSWR<Main>(
-        `https://music.163.com/api/v1/user/detail/${userId || currentUser.id}`,
+  const currentUser = useAppSelector(selectUser);
+  const isDev = useAppSelector(selectDevModeEnabled);
+
+  const { data } = useSWR<Main>(
+    `https://music.163.com/api/v1/user/detail/${userId || currentUser.id}`,
+  );
+
+  const viewAvatar = () => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectDoubleClick
     );
+    if (data) {
+      //@ts-expect-error
+      navigation.push('WebView', {
+        url: data?.profile.avatarUrl,
+        title: 'Avatar',
+      });
+    }
+  };
 
-    const viewAvatar = () => {
-        HapticFeedback.trigger(
-            HapticFeedbackTypes.effectDoubleClick
-        );
-        if (data) {
-            //@ts-expect-error
-            navigation.push('WebView', {
-                url: data?.profile.avatarUrl,
-                title: 'Avatar',
-            });
-        }
-    };
+  const goUser = () => {
+    HapticFeedback.trigger(HapticFeedbackTypes.effectClick);
+    navigation.navigate('UserDetail' as never);
+  };
 
-    const goSwitchUser = () => {
-        HapticFeedback.trigger(HapticFeedbackTypes.effectClick);
-        navigation.navigate('SwitchUser' as never);
-    };
+  const debugPrint = () => {
+    if (isDev) {
+      Alert.alert(
+        'User Info',
+        JSON.stringify(
+          data, null, 2
+        ),
+      );
+    }
+  };
 
-    return (
-        <>
-            <TouchableRipple
-                borderless
-                style={styles.avatar}
-                onPress={goSwitchUser}
-                onLongPress={viewAvatar}
-            >
-                <Avatar.Image
-                    size={70}
-                    source={{ uri: data?.profile.avatarUrl }}
-                />
-            </TouchableRipple>
+  return (
+    <View style={[styles.info, style]}>
+      <TouchableRipple
+        borderless
+        style={styles.avatar}
+        onPress={goUser}
+        onLongPress={viewAvatar}
+      >
+        <Avatar.Image
+          size={70}
+          source={{ uri: data?.profile.avatarUrl }}
+        />
+      </TouchableRipple>
 
-            <Text variant="labelLarge">
-                {data?.profile.nickname}
-            </Text>
-            <Text
-                variant="labelMedium"
-                style={[styles.signature, {
-                    color: appTheme.dark
-                        ? appTheme.colors.onSurfaceDisabled
-                        : appTheme.colors.backdrop,
-                }]}
-            >
-                {data?.profile.signature}
-            </Text>
-        </>
-    );
+      <Text variant="labelLarge" onPress={debugPrint}>
+        {data?.profile.nickname}
+      </Text>
+      <Text
+        variant="labelMedium"
+        style={[styles.signature, {
+          color: appTheme.dark
+            ? appTheme.colors.onSurfaceDisabled
+            : appTheme.colors.backdrop,
+        }]}
+      >
+        {data?.profile.signature}
+      </Text>
+    </View>
+  );
 };
 
-export const UserBackground = ({ userId, children }:
-    PropsWithChildren<{ userId?: number }>
-) => {
-    const navigation = useNavigation();
-    const appTheme = useTheme();
+export const UserBackground = ({
+  userId, children, style
+}: PropsWithChildren<{
+  userId?: number, style?: StyleProp<ImageStyle>
+}>) => {
+  const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+  const appTheme = useTheme();
 
-    const currentUser = useAppSelector(selectUser);
-    const { data, error, isLoading } = useSWR<Main>(
-        `https://music.163.com/api/v1/user/detail/${userId || currentUser.id}`,
-    );
+  const currentUser = useAppSelector(selectUser);
+  const { data, error, isLoading } = useSWR<Main>(
+    `https://music.163.com/api/v1/user/detail/${userId || currentUser.id}`,
+  );
 
-    if (isLoading) {
-        return (
-            <View style={styles.img}>
-                <ActivityIndicator style={styles.loading} />
-            </View>
-        );
-    }
-
-    if (error) {
-        return (
-            <Text style={[
-                styles.errMsg,
-                { color: appTheme.colors.error },
-            ]}>
-                Error: {error.message}
-            </Text>
-        );
-    }
-
-    const viewBackground = () => {
-        HapticFeedback.trigger(
-            HapticFeedbackTypes.effectDoubleClick
-        );
-        if (data) {
-            //@ts-expect-error
-            navigation.push('WebView', {
-                url: data?.profile.backgroundUrl,
-                title: 'User Background',
-            });
-        }
-    };
-
+  if (isLoading) {
     return (
-        <TouchableWithoutFeedback
-            onLongPress={viewBackground}
-        >
-            <ImageBackground
-                style={styles.header}
-                imageStyle={styles.img}
-                source={{
-                    uri: data?.profile.backgroundUrl ||
-                        placeholderImg,
-                }}
-            >
-                {children}
-            </ImageBackground>
-        </TouchableWithoutFeedback>
+      <View style={styles.img}>
+        <ActivityIndicator style={styles.loading} />
+      </View>
     );
+  }
+
+  if (error) {
+    return (
+      <Text style={[styles.errMsg, {
+        color: appTheme.colors.error,
+        marginTop: insets.top
+      }]}>
+        Error: {error.message}
+      </Text>
+    );
+  }
+
+  const viewBackground = () => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectDoubleClick
+    );
+    if (data) {
+      //@ts-expect-error
+      navigation.push('WebView', {
+        url: data?.profile.backgroundUrl,
+        title: 'User Background',
+      });
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onLongPress={viewBackground}
+    >
+      <ImageBackground
+        imageStyle={[styles.img, style]}
+        source={{
+          uri: data?.profile.backgroundUrl ||
+            placeholderImg,
+        }}
+      >
+        {children}
+      </ImageBackground>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export const UserHeader = ({ userId }: { userId?: number }) => {
-    return (
-        <UserBackground userId={userId}>
-            <UserInfo userId={userId} />
-        </UserBackground>
-    );
+  return (
+    <UserBackground userId={userId}>
+      <UserInfo userId={userId} />
+    </UserBackground>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        alignItems: 'center',
-    },
-    img: {
-        height: '60%',
-    },
-    avatar: {
-        marginTop: '30%',
-        borderRadius: 50,
-    },
-    errMsg: {
-        textAlign: 'center',
-        marginTop: StatusBar.currentHeight,
-        alignSelf: 'center',
-    },
-    loading: {
-        flex: 1,
-        alignSelf: 'center',
-    },
-    signature: {
-        marginTop: 10,
-    },
+  img: {
+    height: '75%',
+  },
+  avatar: {
+    borderRadius: 50,
+  },
+  errMsg: {
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  loading: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+  signature: {
+    marginTop: '1%',
+  },
+  info: {
+    alignSelf: 'center',
+    marginTop: '45%',
+    alignItems: 'center',
+  }
 });
