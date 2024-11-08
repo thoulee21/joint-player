53fa8e9da897eb5293fad0010952718aa67bea49
thoulee <thou_lee@outlook.com>
2024-11-08 14:48:50 +0800

    chore(types): 添加搜索歌单相关类型定义

---
+++ src/types/searchPlaylist.d.ts
@@ -0,0 +1,39 @@
+export interface Main {
+  result: Result;
+  code:   number;
+}
+
+export interface Result {
+  playlists:     Playlist[];
+  playlistCount: number;
+}
+
+export interface Playlist {
+  id:            number;
+  name:          string;
+  coverImgUrl:   string;
+  creator:       Creator;
+  subscribed:    boolean;
+  trackCount:    number;
+  userId:        number;
+  playCount:     number;
+  bookCount:     number;
+  specialType:   number;
+  officialTags:  null;
+  action:        null;
+  actionType:    null;
+  recommendText: null;
+  score:         null;
+  description:   null | string;
+  highQuality:   boolean;
+}
+
+export interface Creator {
+  nickname:   string;
+  userId:     number;
+  userType:   number;
+  avatarUrl:  null;
+  authStatus: number;
+  expertTags: null;
+  experts:    null;
+}
