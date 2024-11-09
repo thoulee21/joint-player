7853ec55215bb7b0d74901962d238b144a2bb9ae
thoulee <thou_lee@outlook.com>
2024-11-09 22:24:23 +0800

    feat(AlbumItem): 优化专辑项组件，添加歌曲数量显示和标签，调整样式以改善布局

--- src/components/AlbumItem.tsx
+++ src/components/AlbumItem.tsx
@@ -1,45 +1,81 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import { Card, useTheme } from 'react-native-paper';
+import React, { useCallback } from 'react';
+import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { Caption, Card, Chip, useTheme } from 'react-native-paper';
 import { HotAlbum } from '../types/albumArtist';
 import { toReadableDate } from '../utils';
 
-export const Album = ({ item }: { item: HotAlbum }) => {
-    const navigation = useNavigation();
-    const appTheme = useTheme();
-    const { width: windowWidth } = Dimensions.get('window');
+export const Album = (
+  { item }: { item: HotAlbum }
+) => {
+  const navigation = useNavigation();
+  const appTheme = useTheme();
 
-    return (
-        <Card
-            style={styles.album}
-            onPress={() => {
-                //@ts-expect-error
-                navigation.navigate('AlbumDetail', { album: item });
-            }}
-        >
-            <Card.Cover
-                style={[styles.albumPic, { width: windowWidth / 2 - 20 }]}
-                source={{ uri: item.picUrl }}
-            />
-            <Card.Title
-                title={item.name}
-                subtitle={toReadableDate(item.publishTime)}
-                subtitleStyle={{
-                    color: appTheme.dark
-                        ? appTheme.colors.onSurfaceDisabled
-                        : appTheme.colors.backdrop,
-                }}
-            />
-        </Card>
-    );
+  const window = useWindowDimensions();
+  const width = window.width / 2 - 15;
+
+  const renderSongCount = useCallback(
+    () => (
+      <Caption style={styles.count}>
+        {item.size}
+      </Caption>
+    ), [item.size]);
+
+  return (
+    <Card
+      style={styles.album}
+      onPress={() => {
+        navigation.navigate(
+          //@ts-expect-error
+          'AlbumDetail',
+          { album: item }
+        );
+      }}
+    >
+      <Card.Cover
+        style={[styles.albumPic, { width }]}
+        source={{ uri: item.picUrl }}
+      />
+      <Card.Title
+        title={item.name}
+        subtitle={toReadableDate(item.publishTime)}
+        subtitleStyle={{
+          color: appTheme.dark
+            ? appTheme.colors.onSurfaceDisabled
+            : appTheme.colors.backdrop,
+        }}
+        right={renderSongCount}
+      />
+      <View style={[styles.chips, { width }]}>
+        <Chip compact style={styles.chip}>
+          {item.type}
+        </Chip>
+        <Chip compact style={styles.chip}>
+          {item.subType}
+        </Chip>
+      </View>
+    </Card>
+  );
 };
 
 const styles = StyleSheet.create({
-    albumPic: {
-        height: 200,
-    },
-    album: {
-        margin: '2%',
-    },
+  albumPic: {
+    height: 200,
+  },
+  album: {
+    margin: '1%',
+  },
+  chips: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: '2%',
+    paddingHorizontal: '2%',
+  },
+  chip: {
+    margin: '0.5%',
+  },
+  count: {
+    margin: '5%',
+    fontSize: 15,
+  },
 });
