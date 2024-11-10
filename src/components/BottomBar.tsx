42ab3175204b5e2bcd851df9c2266edbc6de9afa
thoulee <thou_lee@outlook.com>
2024-11-10 13:07:54 +0800

    refactor(BottomBar): 统一代码风格

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -3,69 +3,69 @@ import { useNavigation } from '@react-navigation/native';
 import React, { RefObject } from 'react';
 import { StyleSheet } from 'react-native';
 import HapticFeedback, {
-    HapticFeedbackTypes,
+  HapticFeedbackTypes,
 } from 'react-native-haptic-feedback';
 import { Appbar } from 'react-native-paper';
 import { useActiveTrack } from 'react-native-track-player';
 
 export const BottomBar = ({ bottomSheetRef }:
-    { bottomSheetRef: RefObject<BottomSheetModal> }
+  { bottomSheetRef: RefObject<BottomSheetModal> }
 ) => {
-    const navigation = useNavigation();
-    const track = useActiveTrack();
+  const navigation = useNavigation();
+  const track = useActiveTrack();
 
-    return (
-        <Appbar.Header
-            style={styles.bottomBar}
-            mode="center-aligned"
-        >
-            <Appbar.Action
-                icon="video-outline"
-                disabled={track?.mvid === 0
-                    || typeof track?.mvid === 'undefined'}
-                onPress={() => {
-                    HapticFeedback.trigger(
-                        HapticFeedbackTypes.effectHeavyClick
-                    );
-                    // @ts-ignore
-                    navigation.push('MvDetail');
-                }}
-            />
-            <Appbar.Content
-                title={track?.album || 'No Album'}
-                titleStyle={styles.bottomTitle}
-                onPress={() => {
-                    HapticFeedback.trigger(
-                        HapticFeedbackTypes.effectHeavyClick
-                    );
-                    //@ts-ignore
-                    navigation.push('AlbumDetail', {
-                        album: track?.albumRaw,
-                    });
-                }}
-                disabled={
-typeof track?.albumRaw === 'undefined' 
-|| !track?.album}
-            />
-            <Appbar.Action
-                icon="menu-open"
-                disabled={!track}
-                onPress={() => {
-                    HapticFeedback.trigger(
-                        HapticFeedbackTypes.effectHeavyClick
-                    );
-                    bottomSheetRef.current?.present();
-                }}
-            />
-        </Appbar.Header>
-    );
+  return (
+    <Appbar.Header
+      style={styles.bottomBar}
+      mode="center-aligned"
+    >
+      <Appbar.Action
+        icon="video-outline"
+        disabled={track?.mvid === 0
+          || typeof track?.mvid === 'undefined'}
+        onPress={() => {
+          HapticFeedback.trigger(
+            HapticFeedbackTypes.effectHeavyClick
+          );
+          // @ts-ignore
+          navigation.push('MvDetail');
+        }}
+      />
+      <Appbar.Content
+        title={track?.album || 'No Album'}
+        titleStyle={styles.bottomTitle}
+        onPress={() => {
+          HapticFeedback.trigger(
+            HapticFeedbackTypes.effectHeavyClick
+          );
+          //@ts-ignore
+          navigation.push('AlbumDetail', {
+            album: track?.albumRaw,
+          });
+        }}
+        disabled={
+          typeof track?.albumRaw === 'undefined'
+          || !track?.album}
+      />
+      <Appbar.Action
+        icon="menu-open"
+        disabled={!track}
+        onPress={() => {
+          HapticFeedback.trigger(
+            HapticFeedbackTypes.effectHeavyClick
+          );
+          bottomSheetRef.current?.present();
+        }}
+      />
+    </Appbar.Header>
+  );
 };
 
 const styles = StyleSheet.create({
-    bottomBar: {
-        backgroundColor: 'transparent',
-    },
-    bottomTitle: {
-        fontSize: 16,
-    },
+  bottomBar: {
+    backgroundColor: 'transparent',
+  },
+  bottomTitle: {
+    fontSize: 16,
+  },
 });
