d53c7764759dd0f73d601c3c4503e4668e3f7330
thoulee <thou_lee@outlook.com>
2024-11-10 14:22:59 +0800

    fix(ArtistNames): block navigation to artist album page when there is no artist data available

--- src/components/ArtistNames.tsx
+++ src/components/ArtistNames.tsx
@@ -1,60 +1,106 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback } from 'react';
-import { FlatList, StyleProp, StyleSheet, TextStyle } from 'react-native';
+import {
+  FlatList,
+  StyleProp,
+  StyleSheet,
+  TextStyle,
+  ToastAndroid,
+} from 'react-native';
 import HapticFeedback, {
-    HapticFeedbackTypes,
+  HapticFeedbackTypes,
 } from 'react-native-haptic-feedback';
 import { Text, useTheme } from 'react-native-paper';
 import { useActiveTrack } from 'react-native-track-player';
-import { Artist } from '../types/albumArtist';
+import useSWRInfinite from 'swr/infinite';
+import { Artist, type Main } from '../types/albumArtist';
+
+
+const Name = ({ item, goArtist, textStyle }: {
+  item: Artist,
+  goArtist: (item: Artist) => void,
+  textStyle: StyleProp<TextStyle>,
+}) => {
+  const appTheme = useTheme();
+
+  const { data } = useSWRInfinite<Main>((index) =>
+    `http://music.163.com/api/artist/albums/${item.id}?offset=${index * 10}&limit=10&total=true`,
+  );
+
+  const onPress = useCallback(() => {
+    if (data && data[0].artist) {
+      goArtist(data[0].artist);
+    } else {
+      ToastAndroid.show(
+        'No artist data found',
+        ToastAndroid.SHORT
+      );
+    }
+  }, [data, goArtist]);
+
+  return (
+    <Text
+      onPress={onPress}
+      style={[
+        textStyle,
+        { color: appTheme.colors.primary }
+      ]}
+    >
+      {item.name}
+    </Text>
+  );
+};
 
 export const ArtistNames = ({ textStyle, artists }:
-    { textStyle?: StyleProp<TextStyle>, artists?: Artist[] }
+  { textStyle?: StyleProp<TextStyle>, artists?: Artist[] }
 ) => {
-    const navigation = useNavigation();
-    const appTheme = useTheme();
-    const track = useActiveTrack();
+  const navigation = useNavigation();
+  const appTheme = useTheme();
+  const track = useActiveTrack();
 
-    const themedGray = appTheme.dark
-        ? appTheme.colors.onSurfaceDisabled
-        : appTheme.colors.backdrop;
+  const themedGray = appTheme.dark
+    ? appTheme.colors.onSurfaceDisabled
+    : appTheme.colors.backdrop;
 
-    const goArtist = (item: Artist) => {
-        HapticFeedback.trigger(
-            HapticFeedbackTypes.effectHeavyClick
-        );
-        //@ts-ignore
-        navigation.push('Artist', {
-            artist: item,
-        });
-    };
-
-    const renderSeparator = useCallback(() => (
-        <Text style={[textStyle, { color: themedGray }]}>/</Text>
-    ), [textStyle, themedGray]);
-
-    return (
-        <FlatList
-            data={artists || track?.artists as Artist[]}
-            horizontal
-            style={styles.names}
-            showsHorizontalScrollIndicator={false}
-            keyExtractor={(item) => item.id.toString()}
-            ItemSeparatorComponent={renderSeparator}
-            renderItem={({ item }) => (
-                <Text
-                    style={[textStyle, {
-                        color: appTheme.colors.primary,
-                    }]}
-                    onPress={() => goArtist(item)}
-                >{item.name}</Text>
-            )}
-        />
+  const goArtist = useCallback((item: Artist) => {
+    HapticFeedback.trigger(
+      HapticFeedbackTypes.effectHeavyClick
     );
+    //@ts-ignore
+    navigation.push('Artist', {
+      artist: item,
+    });
+  }, [navigation]);
+
+  const renderSeparator = useCallback(() => (
+    <Text style={[textStyle, { color: themedGray }]}>/</Text>
+  ), [textStyle, themedGray]);
+
+  const renderItem = useCallback((
+    { item }: { item: Artist }
+  ) => (
+    <Name
+      item={item}
+      goArtist={goArtist}
+      textStyle={textStyle}
+    />
+  ), [goArtist, textStyle]);
+
+  return (
+    <FlatList
+      data={artists || track?.artists as Artist[]}
+      horizontal
+      style={styles.names}
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item) => item.id.toString()}
+      ItemSeparatorComponent={renderSeparator}
+      renderItem={renderItem}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    names: {
-        maxWidth: '80%',
-    },
+  names: {
+    maxWidth: '80%',
+  },
 });
