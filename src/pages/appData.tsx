bfdd678f024ec6bc07be2691c46cbcf20df20156
thoulee <thou_lee@outlook.com>
2024-11-14 15:06:50 +0800

    feat: 优化 AppDataScreen 组件，添加 LayoutAnimation 以增强切换效果，并调整代码格式

--- src/pages/appData.tsx
+++ src/pages/appData.tsx
@@ -1,10 +1,10 @@
 import {
-    createMaterialTopTabNavigator,
-    MaterialTopTabBarProps,
+  createMaterialTopTabNavigator,
+  MaterialTopTabBarProps,
 } from '@react-navigation/material-top-tabs';
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { LayoutAnimation, StyleSheet } from 'react-native';
 import { Appbar, Icon, SegmentedButtons, useTheme } from 'react-native-paper';
 import packageJson from '../../package.json';
 import { BlurBackground } from '../components/BlurBackground';
@@ -17,162 +17,176 @@ import { storage } from '../utils/reduxPersistMMKV';
 const TopTab = createMaterialTopTabNavigator();
 
 const PackageData = () => {
-    const packageData = Object.keys(packageJson).map((key) => ({
-        name: key,
-        data: (packageJson as Record<string, any>)[key],
-    }));
+  const packageData = Object.keys(packageJson).map((key) => ({
+    name: key,
+    data: (packageJson as Record<string, any>)[key],
+  }));
 
-    return (<DataList dataItems={packageData} />);
+  return <DataList dataItems={packageData} />;
 };
 
 const ReduxState = () => {
-    const state: { [key: string]: any } = store.getState();
-    const stateList = Object.keys(state).map((key) => ({
-        name: key,
-        data: state[key].value || state[key],
-    }));
+  const state: { [key: string]: any } = store.getState();
+  const stateList = Object.keys(state).map((key) => ({
+    name: key,
+    data: state[key].value || state[key],
+  }));
 
-    return (<DataList dataItems={stateList} />);
+  return <DataList dataItems={stateList} />;
 };
 
 const StorageList = () => {
-    const [localData, setLocalData] = useState<DataItemType[]>([]);
+  const [localData, setLocalData] = useState<DataItemType[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const storageFetches = storage.getAllKeys()
-                .map((localDataName) => {
-                    const data = JSON.parse(
-                        storage.getString(localDataName) || ''
-                    );
-                    return {
-                        name: localDataName,
-                        data: data,
-                    };
-                });
-            const dataItems = await Promise.all(storageFetches);
-            setLocalData(dataItems);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const storageFetches = storage.getAllKeys()
+        .map((localDataName) => {
+          const data = JSON.parse(
+            storage.getString(localDataName) || ''
+          );
+          return {
+            name: localDataName,
+            data: data,
+          };
+        });
+      const dataItems = await Promise.all(storageFetches);
+      setLocalData(dataItems);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (<DataList dataItems={localData} />);
+  return <DataList dataItems={localData} />;
 };
 
 export function AppDataScreen() {
-    const appTheme = useTheme();
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const appTheme = useTheme();
 
-    const renderTapBar = useCallback(({
-        state, navigation: topTabNavi, descriptors,
-    }: MaterialTopTabBarProps) => {
-        const tabBarItems = state.routes.map((route, index) => {
-            const { title, tabBarIcon } = descriptors[route.key].options;
-            return {
-                value: state.routeNames[index],
-                label: title,
-                icon: tabBarIcon ? ({ color }: { color: string }) => (
-                    tabBarIcon({ color, focused: state.index === index })
-                ) : undefined,
-                showSelectedCheck: true,
-            };
-        });
+  const [currentRouteIndex, setCurrentRouteIndex] = useState(0);
 
-        return (
-            <SegmentedButtons
-                style={styles.tabBar}
-                value={state.routeNames[state.index]}
-                buttons={tabBarItems}
-                onValueChange={(value) => {
-                    topTabNavi.navigate(value);
-                }}
-                theme={{
-                    ...appTheme,
-                    colors: {
-                        ...appTheme.colors,
-                        secondaryContainer: 'transparent',
-                    },
-                }}
-            />
-        );
-    }, [appTheme]);
-
-    const renderStateIcon = useCallback(
-        ({ color }: { color: string }) => (
-            <Icon
-                source="cellphone-information"
-                color={color}
-                size={20}
-            />
-        ), []);
-
-    const renderStorageIcon = useCallback(
-        ({ color }: { color: string }) => (
-            <Icon
-                source="database-cog-outline"
-                color={color}
-                size={20}
-            />
-        ), []);
-
-    const renderPackageIcon = useCallback(
-        ({ color }: { color: string }) => (
-            <Icon
-                source="package-variant"
-                color={color}
-                size={20}
-            />
-        ), []);
+  const renderTapBar = useCallback(({
+    state, navigation: topTabNavi, descriptors,
+  }: MaterialTopTabBarProps) => {
+    const tabBarItems = state.routes.map(
+      (route, index) => {
+        const { title, tabBarIcon } = descriptors[route.key].options;
+        return {
+          value: state.routeNames[index],
+          label: title,
+          icon: tabBarIcon ? ({ color }: { color: string }) => (
+            tabBarIcon({ color, focused: state.index === index })
+          ) : undefined,
+          showSelectedCheck: true,
+        };
+      }
+    );
 
     return (
-        <BlurBackground>
-            <Appbar.Header style={styles.transparent}>
-                <Appbar.BackAction onPress={navigation.goBack} />
-                <Appbar.Content title="App Data" />
-                <MMKVStorageIndicator />
-            </Appbar.Header>
-
-            <TopTab.Navigator
-                backBehavior="none"
-                screenOptions={{ lazy: true }}
-                tabBar={renderTapBar}
-            >
-                <TopTab.Screen
-                    name="ReduxState"
-                    component={ReduxState}
-                    options={{
-                        title: 'State',
-                        tabBarIcon: renderStateIcon,
-                    }}
-                />
-                <TopTab.Screen
-                    name="LocalStorage"
-                    component={StorageList}
-                    options={{
-                        title: 'Storage',
-                        tabBarIcon: renderStorageIcon,
-                    }}
-                />
-                <TopTab.Screen
-                    name="PackageData"
-                    component={PackageData}
-                    options={{
-                        title: 'Package',
-                        tabBarIcon: renderPackageIcon,
-                    }}
-                />
-            </TopTab.Navigator>
-        </BlurBackground>
+      <SegmentedButtons
+        style={styles.tabBar}
+        value={state.routeNames[state.index]}
+        buttons={tabBarItems}
+        onValueChange={(value) => {
+          topTabNavi.navigate(value);
+        }}
+        theme={{
+          ...appTheme,
+          colors: {
+            ...appTheme.colors,
+            secondaryContainer: 'transparent',
+          },
+        }}
+      />
     );
+  }, [appTheme]);
+
+  const renderStateIcon = useCallback(
+    ({ color }: { color: string }) => (
+      <Icon
+        source="cellphone-information"
+        color={color}
+        size={20}
+      />
+    ), []);
+
+  const renderStorageIcon = useCallback(
+    ({ color }: { color: string }) => (
+      <Icon
+        source="database-cog-outline"
+        color={color}
+        size={20}
+      />
+    ), []);
+
+  const renderPackageIcon = useCallback(
+    ({ color }: { color: string }) => (
+      <Icon
+        source="package-variant"
+        color={color}
+        size={20}
+      />
+    ), []);
+
+  return (
+    <BlurBackground>
+      <Appbar.Header style={styles.transparent}>
+        <Appbar.BackAction onPress={navigation.goBack} />
+        <Appbar.Content title="App Data" />
+
+        {currentRouteIndex === 1 && (
+          <MMKVStorageIndicator />
+        )}
+      </Appbar.Header>
+
+      <TopTab.Navigator
+        backBehavior="none"
+        tabBar={renderTapBar}
+        screenListeners={() => ({
+          state: ({ data }) => {
+            LayoutAnimation.configureNext(
+              LayoutAnimation.Presets.easeInEaseOut
+            );
+            setCurrentRouteIndex(data.state.index);
+          }
+        })}
+      >
+        <TopTab.Screen
+          name="ReduxState"
+          component={ReduxState}
+          options={{
+            title: 'State',
+            tabBarIcon: renderStateIcon,
+          }}
+        />
+        <TopTab.Screen
+          name="LocalStorage"
+          component={StorageList}
+          options={{
+            title: 'Storage',
+            tabBarIcon: renderStorageIcon,
+          }}
+        />
+        <TopTab.Screen
+          name="PackageData"
+          component={PackageData}
+          options={{
+            title: 'Package',
+            tabBarIcon: renderPackageIcon,
+          }}
+        />
+      </TopTab.Navigator>
+    </BlurBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    tabBar: {
-        shadowColor: 'transparent',
-        marginHorizontal: 16,
-    },
-    transparent: {
-        backgroundColor: 'transparent',
-    },
+  tabBar: {
+    shadowColor: 'transparent',
+    marginHorizontal: 16,
+  },
+  transparent: {
+    backgroundColor: 'transparent',
+  },
 });
