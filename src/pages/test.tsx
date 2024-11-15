418b4ec86d850cc150cd0d1356cc3187c64f3a84
thoulee <thou_lee@outlook.com>
2024-11-15 21:34:50 +0800

    feat(TestScreen): 引入 @codeherence/react-native-header 组件并更新页面布局

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,27 +1,52 @@
-import { useNavigation } from '@react-navigation/native';
+import {
+  Header,
+  LargeHeader,
+  ScalingView,
+  ScrollViewWithHeaders,
+} from '@codeherence/react-native-header';
 import type { LocalAuthenticationResult } from 'expo-local-authentication';
 import * as LocalAuthentication from 'expo-local-authentication';
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, Card, Text } from 'react-native-paper';
+import type { SharedValue } from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BlurBackground } from '../components/BlurBackground';
 import { rootLog } from '../utils/logger';
 
+const HeaderComponent = (
+  { showNavBar }: { showNavBar: SharedValue<number> }
+) => (
+  <Header
+    showNavBar={showNavBar}
+    headerCenter={
+      <Text style={styles.headerTitle}>
+        react-native-header
+      </Text>
+    }
+  />
+);
+
+const LargeHeaderComponent = (
+  { scrollY }: { scrollY: SharedValue<number> }
+) => {
+  return (
+    <LargeHeader>
+      <ScalingView scrollY={scrollY}>
+        <Text variant="titleMedium">Welcome!</Text>
+        <Text variant="titleLarge">react-native-header</Text>
+        <Text variant="titleSmall">
+          This project displays some header examples using the package.
+        </Text>
+      </ScalingView>
+    </LargeHeader>
+  );
+};
+
 export const TestScreen = () => {
-  const navigation = useNavigation();
-
+  const { bottom } = useSafeAreaInsets();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [
-    authResult,
-    setAuthResult,
-  ] = useState<LocalAuthenticationResult>();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      title: 'Authentication Test',
-      headerShown: true,
-    });
-  }, [navigation]);
+  const [authResult, setAuthResult] = useState<LocalAuthenticationResult>();
 
   useEffect(() => {
     const init = async () => {
@@ -54,32 +79,44 @@ export const TestScreen = () => {
 
   return (
     <BlurBackground>
-      <Card style={styles.main}>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Title title="Result" />
+      <ScrollViewWithHeaders
+        HeaderComponent={HeaderComponent}
+        LargeHeaderComponent={LargeHeaderComponent}
+        contentContainerStyle={{ paddingBottom: bottom }}
+      >
+        <Card style={styles.main}>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+          <Card.Title title="Result" />
 
-        <Card.Content>
-          <Text>
-            {JSON.stringify(
-              authResult, null, 2
-            )}
-          </Text>
-        </Card.Content>
+          <Card.Content>
+            <Text>
+              {JSON.stringify(
+                authResult, null, 2
+              )}
+            </Text>
+          </Card.Content>
 
-        <Card.Actions>
-          <Button
-            icon="reload"
-            onPress={() => setIsLoaded(false)}
-          >Reload</Button>
-        </Card.Actions>
-      </Card>
+          <Card.Actions>
+            <Button
+              icon="reload"
+              onPress={() => setIsLoaded(false)}
+            >Reload</Button>
+          </Card.Actions>
+        </Card>
+      </ScrollViewWithHeaders>
     </BlurBackground>
   );
 };
 
 const styles = StyleSheet.create({
   main: {
+    flex: 1,
+    height: 1500,
     marginTop: '10%',
     marginHorizontal: '2.5%',
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
   }
 });
