1cca24b3819390f978c49ecb6fc6ded59cf90125
thoulee <thou_lee@outlook.com>
2024-11-15 15:08:19 +0800

    fix(DataMoreButton): 添加状态栏高度支持

--- src/components/DataMoreButton.tsx
+++ src/components/DataMoreButton.tsx
@@ -1,47 +1,55 @@
 import Clipboard from '@react-native-clipboard/clipboard';
 import React, { useState } from 'react';
-import { Share, ToastAndroid } from 'react-native';
+import { Share, StatusBar, ToastAndroid } from 'react-native';
 import HapticFeedback from 'react-native-haptic-feedback';
 import { IconButton, Menu } from 'react-native-paper';
 
 export const DataMoreBtn = ({ data, props }: {
-    data: any, props: { size: number }
+  data: any, props: { size: number }
 }) => {
-    const [menuVisible, setMenuVisible] = useState(false);
-    return <Menu
-        anchor={
-            <IconButton
-                {...props}
-                icon="dots-vertical"
-                onPress={() => {
-                    HapticFeedback.trigger('effectHeavyClick');
-                    setMenuVisible(true);
-                }}
-            />}
-        visible={menuVisible}
-        onDismiss={() => setMenuVisible(false)}
+  const [menuVisible, setMenuVisible] = useState(false);
+  return (
+    <Menu
+      anchor={
+        <IconButton
+          {...props}
+          icon="dots-vertical"
+          onPress={() => {
+            HapticFeedback.trigger('effectHeavyClick');
+            setMenuVisible(true);
+          }}
+        />}
+      visible={menuVisible}
+      onDismiss={() => setMenuVisible(false)}
+      statusBarHeight={StatusBar.currentHeight}
     >
-        <Menu.Item
-            leadingIcon="clipboard-outline"
-            title="Copy"
-            onPress={() => {
-                Clipboard.setString(JSON.stringify(data));
+      <Menu.Item
+        leadingIcon="clipboard-outline"
+        title="Copy"
+        onPress={() => {
+          Clipboard.setString(JSON.stringify(data));
 
-                HapticFeedback.trigger('effectTick');
-                ToastAndroid.show('Copied to clipboard', ToastAndroid.SHORT);
-                setMenuVisible(false);
-            }}
-        />
-        <Menu.Item
-            leadingIcon="share-outline"
-            title="Share"
-            disabled={typeof data === 'undefined' || data === ''}
-            onPress={() => {
-                Share.share({ message: JSON.stringify(data) });
+          HapticFeedback.trigger('effectTick');
+          ToastAndroid.show(
+            'Copied to clipboard',
+            ToastAndroid.SHORT
+          );
+          setMenuVisible(false);
+        }}
+      />
+      <Menu.Item
+        leadingIcon="share-outline"
+        title="Share"
+        disabled={typeof data === 'undefined' || data === ''}
+        onPress={() => {
+          Share.share({
+            message: JSON.stringify(data)
+          });
 
-                HapticFeedback.trigger('effectTick');
-                setMenuVisible(false);
-            }}
-        />
-    </Menu>;
+          HapticFeedback.trigger('effectTick');
+          setMenuVisible(false);
+        }}
+      />
+    </Menu>
+  );
 };
