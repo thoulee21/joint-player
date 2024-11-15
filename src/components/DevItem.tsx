6a90d7d9be24594544594c58c63d45d6169da2ae
thoulee <thou_lee@outlook.com>
2024-11-15 21:33:12 +0800

    feat(DevItem): 更新开发者选项组件，添加描述并优化类型定义

--- src/components/DevItem.tsx
+++ src/components/DevItem.tsx
@@ -3,23 +3,25 @@ import React, { useCallback } from 'react';
 import { List } from 'react-native-paper';
 import { useAppSelector } from '../hook';
 import { selectDevModeEnabled } from '../redux/slices';
+import type { ListLRProps } from '../types/paperListItem';
 
 export const DevItem = () => {
     const navigation = useNavigation();
     const isDev = useAppSelector(selectDevModeEnabled);
 
-    const CodeTags = useCallback((props: any) => {
-        return <List.Icon {...props} icon="code-tags" />;
-    }, []);
+    const CodeTags = useCallback((props: ListLRProps) => (
+        <List.Icon {...props} icon="code-tags" />
+    ), []);
 
-    const ChevronRight = useCallback((props: any) => {
-        return <List.Icon {...props} icon="chevron-right" />;
-    }, []);
+    const ChevronRight = useCallback((props: ListLRProps) => (
+        <List.Icon {...props} icon="chevron-right" />
+    ), []);
 
     if (isDev) {
         return (
             <List.Item
                 title="Developer Options"
+                description="Access developer settings."
                 left={CodeTags}
                 right={ChevronRight}
                 onPress={() => {
