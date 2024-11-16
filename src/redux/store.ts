81b1babf700b919f84e1082e4ad33c0542d24979
thoulee <thou_lee@outlook.com>
2024-11-16 21:36:53 +0800

    fix(store): 优化持久化配置，为每个切片配置单独的黑名单

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,72 +1,72 @@
 import {
-    Action,
-    ThunkAction,
-    combineReducers,
-    configureStore,
+  Action,
+  ThunkAction,
+  combineReducers,
+  configureStore,
 } from '@reduxjs/toolkit';
 import * as Sentry from '@sentry/react-native';
 import { persistReducer, persistStore } from 'redux-persist';
 import { reduxStorage } from '../utils/reduxPersistMMKV';
 import { StateKeys } from '../utils/storageKeys';
 import {
-    blurRadiusSlice,
-    darkModeSlice,
-    devModeSlice,
-    dimezisBlurSlice,
-    favsSlice,
-    playlistsSlice,
-    queueSlice,
-    rippleEffectsSlice,
-    searchHistorySlice,
-    userSlice,
+  blurRadiusSlice,
+  darkModeSlice,
+  devModeSlice,
+  dimezisBlurSlice,
+  favsSlice,
+  playlistsSlice,
+  queueSlice,
+  rippleEffectsSlice,
+  searchHistorySlice,
+  userSlice,
 } from './slices';
 
-const createPersistConfig = (key: string) => ({
-    key,
-    storage: reduxStorage,
-    blacklist: [
-        StateKeys.DarkMode,
-        StateKeys.DimezisBlur,
-    ]
+const createPersistConfig = (key: string, blacklist: string[] = []) => ({
+  key,
+  storage: reduxStorage,
+  blacklist,
 });
 
 const rootReducers = combineReducers({
-    darkMode: persistReducer(createPersistConfig(StateKeys.DarkMode), darkModeSlice.reducer),
-    blurRadius: persistReducer(createPersistConfig(StateKeys.BlurRadius), blurRadiusSlice.reducer),
-    devMode: persistReducer(createPersistConfig(StateKeys.DevMode), devModeSlice.reducer),
-    queue: persistReducer(createPersistConfig(StateKeys.Queue), queueSlice.reducer),
-    favs: persistReducer(createPersistConfig(StateKeys.Favs), favsSlice.reducer),
-    user: persistReducer(createPersistConfig(StateKeys.User), userSlice.reducer),
-    playlists: persistReducer(createPersistConfig(StateKeys.Playlists), playlistsSlice.reducer),
-    searchHistory: persistReducer(createPersistConfig(StateKeys.SearchHistory), searchHistorySlice.reducer),
-    rippleEffect: persistReducer(createPersistConfig(StateKeys.RippleEffect), rippleEffectsSlice.reducer),
-    dimezisBlur: persistReducer(createPersistConfig(StateKeys.DimezisBlur), dimezisBlurSlice.reducer),
+  darkMode: persistReducer(createPersistConfig(StateKeys.DarkMode), darkModeSlice.reducer),
+  blurRadius: persistReducer(createPersistConfig(StateKeys.BlurRadius), blurRadiusSlice.reducer),
+  devMode: persistReducer(createPersistConfig(StateKeys.DevMode), devModeSlice.reducer),
+  queue: persistReducer(createPersistConfig(StateKeys.Queue), queueSlice.reducer),
+  favs: persistReducer(createPersistConfig(StateKeys.Favs), favsSlice.reducer),
+  user: persistReducer(createPersistConfig(StateKeys.User), userSlice.reducer),
+  playlists: persistReducer(createPersistConfig(StateKeys.Playlists), playlistsSlice.reducer),
+  searchHistory: persistReducer(createPersistConfig(StateKeys.SearchHistory), searchHistorySlice.reducer),
+  rippleEffect: persistReducer(createPersistConfig(StateKeys.RippleEffect), rippleEffectsSlice.reducer),
+  dimezisBlur: persistReducer(createPersistConfig(StateKeys.DimezisBlur), dimezisBlurSlice.reducer),
 });
 
-const persistedReducer = persistReducer(createPersistConfig('root'), rootReducers);
+const persistedReducer = persistReducer(
+  createPersistConfig('root', [StateKeys.DarkMode, StateKeys.DimezisBlur]),
+  rootReducers
+);
 
 const sentryReduxEnhancer = Sentry.createReduxEnhancer();
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    enhancers: (getDefaultEnhancers) => (
-        getDefaultEnhancers().concat(sentryReduxEnhancer)
-    ),
-    middleware: (getDefaultMiddleware) => (
-        getDefaultMiddleware({
-            serializableCheck: false,
-            immutableCheck: false,
-        })
-    ),
+  reducer: persistedReducer,
+  enhancers: (getDefaultEnhancers) => (
+    getDefaultEnhancers().concat(sentryReduxEnhancer)
+  ),
+  middleware: (getDefaultMiddleware) => (
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    })
+  ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
 >;
 
 export const persister = persistStore(store);
