4de8cfc3c864da315ad10e5729ddec133a9465f7
thoulee <thou_lee@outlook.com>
2024-11-16 14:16:38 +0800

    feat(IssueReportItem): 添加描述文本并优化图标组件类型

--- src/components/IssueReportItem.tsx
+++ src/components/IssueReportItem.tsx
@@ -1,27 +1,29 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback } from 'react';
 import { List } from 'react-native-paper';
+import type { ListLRProps } from '../types/paperListItem';
 
 export const IssueReportItem = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const IssueReportIcon = useCallback((props: any) => {
-        return <List.Icon {...props} icon="message-text-outline" />;
-    }, []);
+  const IssueReportIcon = useCallback((props: ListLRProps) => (
+    <List.Icon {...props} icon="message-text-outline" />
+  ), []);
 
-    const ChevronRight = useCallback((props: any) => {
-        return <List.Icon {...props} icon="chevron-right" />;
-    }, []);
+  const ChevronRight = useCallback((props: ListLRProps) => (
+    <List.Icon {...props} icon="chevron-right" />
+  ), []);
 
-    return (
-        <List.Item
-            title="Report Issue"
-            left={IssueReportIcon}
-            right={ChevronRight}
-            onPress={() => {
-                //@ts-expect-error
-                navigation.push('IssueReport');
-            }}
-        />
-    );
+  return (
+    <List.Item
+      title="Report Issue"
+      description="Report an issue or send feedback"
+      left={IssueReportIcon}
+      right={ChevronRight}
+      onPress={() => {
+        //@ts-expect-error
+        navigation.push('IssueReport');
+      }}
+    />
+  );
 };
