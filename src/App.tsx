e6bf8ae5af231f08eb69b1437c5b1acdcad04f3d
thoulee <thou_lee@outlook.com>
2024-11-23 16:25:45 +0800

    refactor(App.tsx): 更新 Sentry 配置以捕获 100% 事务和配置性能分析

--- src/App.tsx
+++ src/App.tsx
@@ -51,11 +51,15 @@ configureReanimatedLogger({
 Sentry.init({
   dsn: 'https://8f255799dc215a920003a9291d1d1e14@o4507198225383424.ingest.de.sentry.io/4507198229184592',
   environment: __DEV__ ? 'development' : 'production',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+  // We recommend adjusting this value in production.
   tracesSampleRate: 1.0,
+  // profilesSampleRate is relative to tracesSampleRate.
+  // Here, we'll capture profiles for 100% of transactions.
+  profilesSampleRate: 1.0,
   attachScreenshot: true,
   attachViewHierarchy: true,
   _experiments: {
-    profilesSampleRate: 1.0,
     replaysSessionSampleRate: 1.0,
     replaysOnErrorSampleRate: 1.0,
   },
