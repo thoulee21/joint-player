350a302aa942236d069ebccc15064d824445d152
thoulee <thou_lee@outlook.com>
2024-11-25 22:54:20 +0800

    feat(SearchHistoryList): 添加长按删除搜索历史功能，优化用户交互体验

--- src/components/SearchHistoryList.tsx
+++ src/components/SearchHistoryList.tsx
@@ -1,10 +1,16 @@
 import Color from 'color';
 import React, { useCallback } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, Animated, StyleSheet, View } from 'react-native';
+import HapticFeedback, {
+  HapticFeedbackTypes,
+} from 'react-native-haptic-feedback';
 import { Chip, IconButton, List, useTheme } from 'react-native-paper';
 import { useAppDispatch, useAppSelector } from '../hook';
-import { clearSearchHistory, selectSearchHistory } from '../redux/slices';
-import { LottieAnimation } from './LottieAnimation';
+import {
+  clearSearchHistory,
+  removeSearchHistory,
+  selectSearchHistory,
+} from '../redux/slices';
 
 export const SearchHistoryList = ({ setKeyword }: {
   setKeyword: (keyword: string) => void
@@ -19,52 +25,62 @@ export const SearchHistoryList = ({ setKeyword }: {
     <Chip
       compact
       mode="outlined"
-      style={[
-        styles.chip, {
-          backgroundColor: Color(
-            appTheme.colors.surface
-          ).fade(0.4).string(),
-        }
-      ]}
+      style={[styles.chip, {
+        backgroundColor:
+          Color(appTheme.colors.surface)
+            .fade(0.4).toString(),
+      }]}
       key={`${index}-${item}`}
-      onPress={() => {
-        setKeyword(item);
+      onPress={() => { setKeyword(item); }}
+      onLongPress={() => {
+        HapticFeedback.trigger(
+          HapticFeedbackTypes.effectHeavyClick
+        );
+        Alert.alert(
+          'Remove search history',
+          'Do you want to remove this search history?',
+          [
+            { text: 'Cancel', style: 'cancel' },
+            {
+              text: 'OK', onPress: () => {
+                dispatch(
+                  removeSearchHistory(item)
+                );
+              },
+            },
+          ]
+        );
       }}
-    >
-      {item}
-    </Chip>
-  ), [appTheme.colors.surface, setKeyword]);
+    >{item}</Chip>
+  ), [appTheme.colors.surface, dispatch, setKeyword]);
 
-  return (
-    <View style={styles.root}>
-      <View style={styles.header}>
-        <List.Subheader
-          style={{ color: appTheme.colors.secondary }}
-        >
-          Search History
-        </List.Subheader>
+  if (searchHistory.length > 0) {
+    return (
+      <View style={styles.root}>
+        <View style={styles.header}>
+          <List.Subheader
+            style={{ color: appTheme.colors.secondary }}
+          >
+            Search History
+          </List.Subheader>
 
-        <IconButton
-          icon="delete-sweep-outline"
-          disabled={searchHistory.length === 0}
-          onPress={() => {
-            dispatch(clearSearchHistory());
-          }}
-        />
-      </View>
-
-      <View style={styles.list}>
-        {searchHistory.length > 0 ? (
-          searchHistory.map(renderItem)
-        ) : (
-          <LottieAnimation
-            animation="rocket"
-            caption="No search history"
+          <IconButton
+            icon="delete-sweep-outline"
+            disabled={searchHistory.length === 0}
+            onPress={() => {
+              dispatch(clearSearchHistory());
+            }}
           />
-        )}
+        </View>
+
+        <Animated.ScrollView>
+          <View style={styles.list}>
+            {searchHistory.map(renderItem)}
+          </View>
+        </Animated.ScrollView>
       </View>
-    </View>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
