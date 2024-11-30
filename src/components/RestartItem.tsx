7a2c76e5b771aee92cf8409e84053322ad0e3727
thoulee <thou_lee@outlook.com>
2024-11-30 21:35:27 +0800

    refactor(RestartItem): 美化对话框确认重启应用，增强用户交互体验

--- src/components/RestartItem.tsx
+++ src/components/RestartItem.tsx
@@ -1,33 +1,58 @@
-import React, { useCallback } from 'react';
-import { Alert } from 'react-native';
-import { List } from 'react-native-paper';
+import React, { useCallback, useState } from 'react';
+import { Button, Dialog, List, Portal, Text, useTheme } from 'react-native-paper';
 import RNRestart from 'react-native-restart';
+import type { ListLRProps } from '../types/paperListItem';
+import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
 
 export const RestartItem = () => {
-  const restartApp = () => {
-    Alert.alert(
-      'Restart App',
-      'Are you sure you want to restart the app?',
-      [
-        { text: 'Cancel', style: 'cancel' },
-        {
-          text: 'OK',
-          onPress: () => RNRestart.Restart()
-        }
-      ]
-    );
-  };
+  const appTheme = useTheme();
 
-  const renderRestartIcon = useCallback((props: any) => (
+  const [
+    dialogVisible,
+    setDialogVisible
+  ] = useState(false);
+
+  const renderRestartIcon = useCallback((
+    props: ListLRProps
+  ) => (
     <List.Icon {...props} icon="restart" />
   ), []);
 
   return (
-    <List.Item
-      title="Restart App"
-      description="Restart the app to apply changes"
-      left={renderRestartIcon}
-      onPress={restartApp}
-    />
+    <>
+      <List.Item
+        title="Restart App"
+        description="Restart the app to apply changes"
+        left={renderRestartIcon}
+        onPress={() => {
+          HapticFeedback.trigger(
+            HapticFeedbackTypes.notificationWarning
+          );
+          setDialogVisible(true);
+        }}
+      />
+      <Portal>
+        <Dialog
+          visible={dialogVisible}
+          onDismiss={() => setDialogVisible(false)}
+        >
+          <Dialog.Icon icon="alert" size={40} />
+          <Dialog.Title>Restart App</Dialog.Title>
+          <Dialog.Content>
+            <Text>Are you sure you want to restart the app?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              textColor={appTheme.colors.outline}
+              onPress={() => setDialogVisible(false)}
+            >Cancel</Button>
+            <Button
+              textColor={appTheme.colors.error}
+              onPress={() => RNRestart.Restart()}
+            >OK</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
   );
 };
