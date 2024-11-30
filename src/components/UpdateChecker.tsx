1cd461ee59c5a14c443b83570b092a713252fddc
thoulee <thou_lee@outlook.com>
2024-11-30 22:33:59 +0800

    refactor(UpdateChecker): 美化更新对话框，优化用户更新体验

--- src/components/UpdateChecker.tsx
+++ src/components/UpdateChecker.tsx
@@ -1,8 +1,25 @@
 import * as Updates from 'expo-updates';
-import React, { useCallback } from 'react';
-import { Alert, DeviceEventEmitter, ToastAndroid } from 'react-native';
-import HapticFeedback, { HapticFeedbackTypes } from 'react-native-haptic-feedback';
-import { ActivityIndicator, List, useTheme } from 'react-native-paper';
+import React, {
+  useCallback,
+  useState,
+} from 'react';
+import {
+  Alert,
+  DeviceEventEmitter,
+  ToastAndroid,
+} from 'react-native';
+import HapticFeedback, {
+  HapticFeedbackTypes,
+} from 'react-native-haptic-feedback';
+import {
+  ActivityIndicator,
+  Button,
+  Dialog,
+  List,
+  Portal,
+  Text,
+  useTheme,
+} from 'react-native-paper';
 import RNRestart from 'react-native-restart';
 import useSWR from 'swr';
 import packageData from '../../package.json';
@@ -11,14 +28,18 @@ import { selectDevModeEnabled } from '../redux/slices';
 import type { Main } from '../types/latestRelease';
 import { rootLog } from '../utils/logger';
 
+const USER_REPO = packageData.homepage
+  .split('/')
+  .slice(-2)
+  .join('/');
+
 export const UpdateChecker = () => {
   const appTheme = useTheme();
   const isDev = useAppSelector(selectDevModeEnabled);
-
-  const userRepo = packageData.homepage.split('/').slice(-2).join('/');
-  const { data } = useSWR<Main>(`https://api.github.com/repos/${userRepo}/releases/latest`);
-  const latestRelease = data?.tag_name;
-  const isLatest = latestRelease === `v${packageData.version}`;
+  const [
+    dialogVisible,
+    setDialogVisible,
+  ] = useState(false);
 
   const {
     currentlyRunning,
@@ -29,18 +50,36 @@ export const UpdateChecker = () => {
     availableUpdate,
   } = Updates.useUpdates();
 
+  const { data } = useSWR<Main>(
+    `https://api.github.com/repos/${USER_REPO}/releases/latest`
+  );
+
+  const showUpdateDialog = useCallback(() => {
+    setDialogVisible(true);
+  }, []);
+
+  const latestRelease = data?.tag_name;
+  const isLatest = latestRelease === `v${packageData.version}`;
+
   const showCurrent = useCallback(() => {
     if (isDev) {
-      HapticFeedback.trigger(HapticFeedbackTypes.effectClick);
+      HapticFeedback.trigger(
+        HapticFeedbackTypes.effectClick
+      );
+
       if (availableUpdate) {
         Alert.alert(
           'Available update info',
-          JSON.stringify(availableUpdate, null, 2)
+          JSON.stringify(
+            availableUpdate, null, 2
+          )
         );
       } else {
         Alert.alert(
           'Current update info',
-          JSON.stringify(currentlyRunning, null, 2)
+          JSON.stringify(
+            currentlyRunning, null, 2
+          )
         );
       }
     }
@@ -51,6 +90,7 @@ export const UpdateChecker = () => {
       await Updates.fetchUpdateAsync();
       RNRestart.Restart();
     } catch (err) {
+      rootLog.error(err);
       ToastAndroid.show(
         `Error updating app: ${JSON.stringify(err)}`,
         ToastAndroid.LONG
@@ -58,47 +98,33 @@ export const UpdateChecker = () => {
     }
   };
 
-  const performUpdateAlert = useCallback(() => {
-    Alert.alert(
-      availableUpdate?.createdAt
-        ? `New update available:\n${availableUpdate?.createdAt.toLocaleString()}`
-        : 'New update available',
-      'Do you want to update now?',
-      [
-        { text: 'Cancel' },
-        {
-          text: 'OK',
-          onPress: isUpdatePending
-            ? () => RNRestart.Restart()
-            : fetchUpdateAndRestart,
-        },
-      ]
-    );
-  }, [availableUpdate?.createdAt, isUpdatePending]);
-
   const checkForUpdate = async () => {
     try {
       const updateCheckRes = await Updates.checkForUpdateAsync();
 
       if (updateCheckRes.isAvailable) {
-        performUpdateAlert();
+        showUpdateDialog();
       } else {
-        ToastAndroid.show('No updates available', ToastAndroid.SHORT);
+        ToastAndroid.show(
+          'No updates available',
+          ToastAndroid.SHORT
+        );
       }
     } catch (err) {
-      Alert.alert(
+      rootLog.error(err);
+      ToastAndroid.show(
         'Error checking for updates',
-        JSON.stringify(err, null, 2)
+        ToastAndroid.SHORT
       );
     }
   };
 
-  const handleUpdatePress = isLatest ?
-    isUpdatePending ? performUpdateAlert : checkForUpdate
-    : () => {
-      DeviceEventEmitter.emit('newReleaseAvailable');
-      rootLog.debug('New release available:', latestRelease);
-    };
+  const handleUpdatePress = isLatest ? (
+    isUpdatePending ? showUpdateDialog : checkForUpdate
+  ) : () => {
+    DeviceEventEmitter.emit('newReleaseAvailable');
+    rootLog.debug('New release available:', latestRelease);
+  };
 
   const description = isDownloading
     ? 'Downloading update...'
@@ -130,14 +156,48 @@ export const UpdateChecker = () => {
   ), [isProcessing]);
 
   return (
-    <List.Item
-      title="Check for updates"
-      description={description}
-      onPress={handleUpdatePress}
-      onLongPress={showCurrent}
-      disabled={isProcessing}
-      left={renderUpdateIcon}
-      right={renderActivityIndicator}
-    />
+    <>
+      <List.Item
+        title="Check for updates"
+        description={description}
+        onPress={handleUpdatePress}
+        onLongPress={showCurrent}
+        disabled={isProcessing}
+        left={renderUpdateIcon}
+        right={renderActivityIndicator}
+      />
+
+      <Portal>
+        <Dialog
+          visible={dialogVisible}
+          onDismiss={() => setDialogVisible(false)}
+        >
+          <Dialog.Icon icon="information" size={40} />
+          <Dialog.Title>Update available</Dialog.Title>
+          <Dialog.Content>
+            <Text>
+              {availableUpdate?.createdAt
+                && `New update created at ${availableUpdate?.createdAt.toLocaleString()} is available.\n`}
+              Do you want to update now?
+            </Text>
+          </Dialog.Content>
+
+          <Dialog.Actions>
+            <Button
+              textColor={appTheme.colors.outline}
+              onPress={() => setDialogVisible(false)}
+            >Cancel</Button>
+
+            <Button
+              onPress={
+                isUpdatePending
+                  ? () => RNRestart.Restart()
+                  : fetchUpdateAndRestart
+              }
+            >Update</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
   );
 };
