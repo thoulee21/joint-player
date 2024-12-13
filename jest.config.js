fbefe197385c734d2f8373211ab46e5e405d48ec
thoulee <thou_lee@outlook.com>
2024-12-13 22:11:18 +0800

    refactor(tests): 更新Jest配置，添加模块文件扩展名和设置文件以支持新功能

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-paper|@react-navigation|react-redux|react-native-track-player|react-native-localize|@sentry/react-native|react-native-vector-icons|react-native-fs)/)',
+    'node_modules/(?!(react-native|react-native-reanimated|react-native-swipeable-item|@react-native|react-native-paper|@react-navigation|react-redux|react-native-track-player|react-native-localize|@sentry/react-native|react-native-vector-icons|react-native-fs)/)',
   ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     '^.+\\.ts?$': 'ts-jest',
   },
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
   },
