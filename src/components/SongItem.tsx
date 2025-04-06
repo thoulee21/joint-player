c297a66255df9c07398e2981fdb33012c2b02ddd
thoulee <thou_lee@outlook.com>
2025-04-06 16:16:51 +0800

    fix: 使用 memo 优化 SongItem 组件性能

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,5 +1,5 @@
 import Color from "color";
-import React, { useCallback, useMemo } from "react";
+import React, { memo, useCallback, useMemo } from "react";
 import { ViewStyle } from "react-native";
 import HapticFeedback, {
   HapticFeedbackTypes,
@@ -12,98 +12,99 @@ import { TrackType } from "../services/GetTracksService";
 import type { ListLRProps } from "../types/paperListItem";
 import { IndexOfSong } from "./IndexOfSong";
 
-export const SongItem = ({
-  item,
-  index,
-  style,
-  showAlbum,
-  showIndex,
-  drag,
-  isActive,
-}: {
-  item: TrackType;
-  index: number;
-  style?: ViewStyle;
-  showAlbum?: boolean;
-  showIndex?: boolean;
-  drag?: () => void;
-  isActive?: boolean;
-}) => {
-  const dispatch = useAppDispatch();
-  const appTheme = useTheme();
+export const SongItem = memo(
+  ({
+    item,
+    index,
+    style,
+    showAlbum,
+    showIndex,
+    drag,
+    isActive,
+  }: {
+    item: TrackType;
+    index: number;
+    style?: ViewStyle;
+    showAlbum?: boolean;
+    showIndex?: boolean;
+    drag?: () => void;
+    isActive?: boolean;
+  }) => {
+    const dispatch = useAppDispatch();
+    const appTheme = useTheme();
 
-  const songStyle = useMemo(
-    () => [
-      {
-        backgroundColor: isActive
-          ? Color(appTheme.colors.secondaryContainer).fade(0.1).string()
-          : appTheme.colors.surface,
-      },
-      style,
-    ],
-    [style, isActive, appTheme],
-  );
+    const songStyle = useMemo(
+      () => [
+        {
+          backgroundColor: isActive
+            ? Color(appTheme.colors.secondaryContainer).fade(0.1).string()
+            : appTheme.colors.surface,
+        },
+        style,
+      ],
+      [style, isActive, appTheme]
+    );
 
-  const play = useCallback(async () => {
-    HapticFeedback.trigger(HapticFeedbackTypes.effectHeavyClick);
-    await dispatch(clearAddOneAsync(item));
-    await TrackPlayer.play();
-  }, [dispatch, item]);
+    const play = useCallback(async () => {
+      HapticFeedback.trigger(HapticFeedbackTypes.effectHeavyClick);
+      await dispatch(clearAddOneAsync(item));
+      await TrackPlayer.play();
+    }, [dispatch, item]);
 
-  const renderIndex = useCallback(
-    (props: ListLRProps) => <IndexOfSong {...props} index={index} />,
-    [index],
-  );
+    const renderIndex = useCallback(
+      (props: ListLRProps) => <IndexOfSong {...props} index={index} />,
+      [index]
+    );
 
-  const description = useMemo(() => {
-    const artists = item.artists.map((ar) => ar.name).join(", ");
+    const description = useMemo(() => {
+      const artists = item.artists.map((ar) => ar.name).join(", ");
 
-    if (showAlbum) {
-      return artists.concat("\n", item.album);
-    } else {
-      return artists;
-    }
-  }, [item.album, item.artists, showAlbum]);
+      if (showAlbum) {
+        return artists.concat("\n", item.album);
+      } else {
+        return artists;
+      }
+    }, [item.album, item.artists, showAlbum]);
 
-  const renderMusicImage = useCallback(
-    ({ color, style: listStyle }: ListLRProps) => (
-      <List.Image
-        style={[
-          listStyle,
-          {
-            borderRadius: appTheme.roundness,
-            backgroundColor: color,
-          },
-        ]}
-        source={{ uri: item.artwork }}
-      />
-    ),
-    [appTheme.roundness, item.artwork],
-  );
-
-  const renderDragIndicator = useCallback(
-    (props: ListLRProps) =>
-      drag && (
-        <IconButton
-          {...props}
-          icon="drag"
-          size={24}
-          onLongPress={drag}
-          testID="drag-handle"
+    const renderMusicImage = useCallback(
+      ({ color, style: listStyle }: ListLRProps) => (
+        <List.Image
+          style={[
+            listStyle,
+            { borderRadius: appTheme.roundness, backgroundColor: color },
+          ]}
+          source={{ uri: item.artwork }}
         />
       ),
-    [drag],
-  );
+      [appTheme.roundness, item.artwork]
+    );
 
-  return (
-    <List.Item
-      title={item.title}
-      left={showIndex ? renderIndex : renderMusicImage}
-      right={renderDragIndicator}
-      description={description}
-      descriptionNumberOfLines={2}
-      onPress={play}
-      style={songStyle}
-    />
-  );
-};
+    const renderDragIndicator = useCallback(
+      (props: ListLRProps) =>
+        drag && (
+          <IconButton
+            {...props}
+            icon="drag"
+            size={24}
+            onLongPress={drag}
+            testID="drag-handle"
+          />
+        ),
+      [drag]
+    );
+
+    return (
+      <List.Item
+        title={item.title}
+        left={showIndex ? renderIndex : renderMusicImage}
+        right={renderDragIndicator}
+        description={description}
+        descriptionNumberOfLines={2}
+        onPress={play}
+        style={songStyle}
+      />
+    );
+  }
+);
+
+SongItem.displayName = "SongItem";
