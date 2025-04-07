9f3070eda6f4417c6e5a7d8aed044fe146133c40
thoulee <thou_lee@outlook.com>
2025-04-07 20:40:15 +0800

    fix(SplashScreen): 隐藏 expo 默认启动界面

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import { registerRootComponent } from "expo";
+import * as SplashScreen from "expo-splash-screen";
 import "react-native-gesture-handler";
 import "react-native-get-random-values";
 
 import App from "./src/App";
+
+SplashScreen.setOptions({ duration: 0 });
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
